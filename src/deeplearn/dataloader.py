22d686c88c0a33a15eb2dff76e9626c3716414b8
ke0m <joseph.s.jennings@gmail.com>
2020-01-03 17:42:33 -0800

    Fixed version number and added generator class

---
+++ src/deeplearn/dataloader.py
@@ -0,0 +1,22 @@
+# Classes/functions for loading in data for deep learning
+from tensorflow.keras.utils import Sequence
+import h5py
+import numpy as np
+
+class resmig_generator_h5(Sequence):
+
+  def __init__(self, fname, batch_size):
+    self.hfin = h5py.File(fname,'r')
+    self.hfkeys = list(hfin.keys())
+    self.nb = int(len(self.hfkeys)/2) # Half is features, half is labels
+    self.indices = np.arange(self.nb)
+    self.batch_size = batch_size
+
+  def __len__(self):
+    return self.nb
+
+  def __getitem__(self,idx):
+    xb = self.hfin[self.hfkeys[idx]]
+    yb = self.hfin[self.hfkeys[idx + self.nb]]
+    return xb, yb
+
