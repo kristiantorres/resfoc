ef08edf4c7f430d515519f805d51029a55b650b4
ke0m <joseph.s.jennings@gmail.com>
2020-01-31 22:16:21 -0800

    Fixed comment to laplacian

--- velocity/mdlbuild.py
+++ velocity/mdlbuild.py
@@ -101,7 +101,7 @@ class mdlbuild:
     # Update layer and velocity models
     self.vel = velot
     self.lyr = lyrot
-    # Compute z-derivative of label
+    # Compute laplacian of label
     self.ec8.laplacian(nz,lbltp,lblot)
     # Apply a threshold
     idx = np.abs(lblot) > thresh
