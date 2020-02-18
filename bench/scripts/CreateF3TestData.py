079c94bf401967059e1b186f7a03d42347b41258
Joseph Jennings <joseph.s.jennings@gmail.com>
2020-02-18 15:35:48 -0800

    Merging from bench-rojas. Starting to create new training data

---
+++ bench/scripts/CreateF3TestData.py
@@ -0,0 +1,122 @@
+"""
+Creates patched images from F3 seismic cube
+
+@author: Joseph Jennings
+@version: 2020.02.18
+"""
+import sys, os, argparse, configparser
+import inpout.seppy as seppy
+import segyio
+import h5py
+import numpy as np
+from deeplearn.python_patch_extractor.PatchExtractor import PatchExtractor
+import deeplearn.utils as dlut
+from utils.ptyprint import create_inttag, progressbar
+import matplotlib.pyplot as plt
+
+# Parse the config file
+conf_parser = argparse.ArgumentParser(add_help=False)
+conf_parser.add_argument("-c", "--conf_file",
+                         help="Specify config file", metavar="FILE")
+args, remaining_argv = conf_parser.parse_known_args()
+defaults = {
+    "verb": "n",
+    "ny": 500,
+    "nto": 256,
+    "nxo": 1024,
+    "ptchz": 128,
+    "ptchx": 128,
+    "strdz": 64,
+    "strdx": 64,
+    "norm": 'y',
+    "wrtcube": 'y',
+    "fs": 0,
+    }
+if args.conf_file:
+  config = configparser.ConfigParser()
+  config.read([args.conf_file])
+  defaults = dict(config.items("defaults"))
+
+# Parse the other arguments
+# Don't surpress add_help here so it will handle -h
+parser = argparse.ArgumentParser(parents=[conf_parser],description=__doc__,
+    formatter_class=argparse.RawDescriptionHelpFormatter)
+
+# Set defaults
+parser.set_defaults(**defaults)
+
+# Input files
+ioArgs = parser.add_argument_group('Inputs and outputs')
+ioArgs.add_argument("in=",help="Input segyfile",type=str)
+ioArgs.add_argument("out=",help="Output H5 file",type=str)
+# Optional arguments
+parser.add_argument("-fs",help="First time sample from which to window the data [0]",type=int)
+parser.add_argument("-ny",help="Y dimension of output 3D cube [500]",type=int)
+parser.add_argument("-nto",help="Interpolate the entire image before patch extraction [512]",type=int)
+parser.add_argument("-nxo",help="Interpolate the entire image before patch extraction [1024]",type=int)
+parser.add_argument("-ptchz",help="Size of patch in z [128]",type=int)
+parser.add_argument("-ptchx",help="Size of patch in x [128]",type=int)
+parser.add_argument("-strdz",help="Patch stride (overlap) in z  [64]",type=int)
+parser.add_argument("-strdx",help="Patch stride (overlap) in x [64]",type=int)
+parser.add_argument("-norm",help="Normalize each patch from -1 to 1 [y]",type=str)
+parser.add_argument("-wrtcube",help="Write out the cube built from the segy [y]",type=str)
+parser.add_argument("-verb",help="Verbosity flag (y or [n])",type=str)
+args = parser.parse_args(remaining_argv)
+
+# Set up SEP
+sep = seppy.sep(sys.argv)
+
+# Get command line arguments
+verb  = sep.yn2zoo(args.verb)
+wrtcub = sep.yn2zoo(args.wrtcube)
+
+# Read in the segy
+with segyio.open(sep.get_fname("in"),ignore_geometry=True) as f: 
+  data = f.trace.raw[:]
+  dt = f.attributes(segyio.TraceField.TRACE_SAMPLE_INTERVAL)[0][0]/1e6
+  dx = (f.attributes(segyio.TraceField.CDP_X)[1][0] - f.attributes(segyio.TraceField.CDP_X)[0][0])/1000.0
+  dy = (f.attributes(segyio.TraceField.CDP_Y)[1][0] - f.attributes(segyio.TraceField.CDP_Y)[0][0])/1000.0
+
+# Reshape the data so it is a regular cube
+fs = args.fs
+nt = data.shape[1] - fs; nx = 951; ny = args.ny
+ntr = nx*ny
+datawind = data[:ntr,fs:]
+datawind = datawind.reshape([ny,nx,nt])
+
+# Patch the interpolated slices
+nzp = args.ptchz; nxp = args.ptchx
+pshape = (nzp,nxp)
+pstride = (args.strdz,args.strdx)
+pe = PatchExtractor(pshape,stride=pstride)
+
+# Open the H5 file
+hf = h5py.File(sep.get_fname("out"),'w')
+
+# Extract the desired slices, interpolate and patch
+nxo = args.nxo; nto = args.nto
+dout = np.zeros([nto,nxo,ny])
+k = 0
+for iy in progressbar(range(ny), "xline: "):
+  dout[:,:,k] = (dlut.resample(datawind[iy,:,:],[nxo,nto],kind='cubic')).T
+  # Extract the patches
+  iptch = pe.extract(dout[:,:,k])
+  # Flatten
+  pz = iptch.shape[0]; px = iptch.shape[1]
+  iptch = iptch.reshape([pz*px,nzp,nxp])
+  niptch = np.zeros(iptch.shape)
+  for ip in range(pz*px):
+    niptch[ip,:,:] = dlut.normalize(iptch[ip,:,:])
+  datatag = create_inttag(k,ny)
+  # Save to dataset
+  hf.create_dataset("x"+datatag, (pz*px,nzp,nxp,1), data=np.expand_dims(niptch,axis=-1), dtype=np.float32)
+  k += 1
+
+# Close the H5 file
+hf.close()
+
+# Write out the cube if desired
+if(wrtcub):
+  axes = seppy.axes([nt,nx,ny],[0.0,0.0,0.0],[dt,dx,dy])
+  sep.write_file(None,axes,datawind.T,ofname='f3cube.H')
+
---
+++ bench/scripts/create_random_faultvel.py
@@ -0,0 +1,227 @@
+"""
+Creates heavily faulted and folded pseudo random velocity models.
+Based on the software by Bob Clapp
+
+@author: Joseph Jennings
+@version: 2020.02.09
+"""
+import sys, os, argparse, configparser
+import inpout.seppy as seppy
+import numpy as np
+import velocity.mdlbuild as mdlbuild
+from scaas.wavelet import ricker
+from utils.ptyprint import progressbar
+import utils.rand as rndut
+import deeplearn.utils as dlut
+from scipy.ndimage import gaussian_filter
+from utils.signal import bandpass
+
+# Parse the config file
+conf_parser = argparse.ArgumentParser(add_help=False)
+conf_parser.add_argument("-c", "--conf_file",
+                         help="Specify config file", metavar="FILE")
+args, remaining_argv = conf_parser.parse_known_args()
+defaults = {
+    "nx": 1000,
+    "ox": 0.0,
+    "dx": 25.0,
+    "ny": 200,
+    "oy": 0.0,
+    "dy": 25.0,
+    "nz": 1000,
+    "oz": 0.0,
+    "dz": 12.5,
+    "rect": 0.5,
+    "nmodels": 1,
+    "verb": "y",
+    "nxo": 1000,
+    "nzo": 1000,
+    "slcy": 100,
+    }
+if args.conf_file:
+  config = configparser.ConfigParser()
+  config.read([args.conf_file])
+  defaults = dict(config.items("defaults"))
+
+# Parse the other arguments
+# Don't surpress add_help here so it will handle -h
+parser = argparse.ArgumentParser(parents=[conf_parser],description=__doc__,
+    formatter_class=argparse.RawDescriptionHelpFormatter)
+
+parser.set_defaults(**defaults)
+
+# Input/output
+ioArgs = parser.add_argument_group("Input output arguments")
+ioArgs.add_argument("-outdir",help="Output directory for header and par files",type=str)
+ioArgs.add_argument("-datapath",help="Output directory for SEP data files",type=str)
+ioArgs.add_argument("-beg",help="Beginning velocity model number",type=int)
+ioArgs.add_argument("-end",help="Ending velocity model number",type=int)
+ioArgs.add_argument("-prefix",help="Prefix for writing velocity models",type=str)
+ioArgs.add_argument("-nmodels",help="Number of output models",type=int)
+# Velocity model arguments
+velArgs = parser.add_argument_group("Velocity model parameters")
+velArgs.add_argument("-nx",help="Number of x samples [1000]",type=int)
+velArgs.add_argument("-ox",help="x origin [0.0]",type=float)
+velArgs.add_argument("-dx",help="x sample spacing [25.0]",type=float)
+velArgs.add_argument("-ny",help="Number of y samples [1000]",type=int)
+velArgs.add_argument("-oy",help="y origin [0.0]",type=float)
+velArgs.add_argument("-dy",help="y sample spacing [25.0]",type=float)
+velArgs.add_argument("-nz",help="Number of z samples [1000]",type=int)
+velArgs.add_argument("-oz",help="z origin [0.0]",type=float)
+velArgs.add_argument("-dz",help="z sample spacing [12.5]",type=float)
+# Processing arguments
+prcArgs = parser.add_argument_group("Velocity model processing")
+prcArgs.add_argument("-nzo",help="Ouput number of depth samples for interpolation [1000]",type=int)
+prcArgs.add_argument("-nxo",help="Ouput number of lateral samples for interpolation [1000]",type=int)
+prcArgs.add_argument("-slcy",help="Index at which to slice the velocity model in y [100]",type=int)
+prcArgs.add_argument("-rect",help="Window radius for smoother [0.5]",type=float)
+parser.add_argument("-verb",help="Verbosity flag ([y] or n)")
+args = parser.parse_args(remaining_argv)
+
+# Set up SEP
+sep = seppy.sep(sys.argv)
+
+## Get commandline arguments
+# Inputs and outputs
+beg     = args.beg
+end     = args.end
+outdir  = args.outdir
+outmod  = outdir + "/" + "velfltmod" + sep.create_inttag(beg,end) + ".H"
+outlbl  = outdir + "/" + "velfltlbl" + sep.create_inttag(beg,end) + ".H"
+outref  = outdir + "/" + "velfltref" + sep.create_inttag(beg,end) + ".H"
+outimg  = outdir + "/" + "velfltimg" + sep.create_inttag(beg,end) + ".H"
+prefix  = args.prefix
+nmodels = args.nmodels
+
+# Get the parameters of the model
+nx = args.nx; dx = args.dx
+ny = args.ny; dy = args.dy
+nz = args.nz; dz = args.dz
+
+# Parameters for ricker wavelet
+nt = 250; ot = 0.0; dt = 0.001; ns = int(nt/2)
+amp = 1.0; dly = 0.125
+minf = 30.0; maxf = 60.0
+
+# Output nz and nx
+nzo = args.nzo; nxo = args.nxo
+slcy = args.slcy
+
+# Output velocity and labels
+vels = np.zeros([nzo,nxo,nmodels],dtype='float32')
+lbls = np.zeros([nzo,nxo,nmodels],dtype='float32')
+refs = np.zeros([nzo,nxo,nmodels],dtype='float32')
+imgs = np.zeros([nzo,nxo,nmodels],dtype='float32')
+
+# Loop over models
+for imodel in range(nmodels):
+  print("Model %d/%d"%(imodel+1,nmodels))
+
+  # Model building object
+  mb = mdlbuild.mdlbuild(nx,dx,ny,dy,dz,basevel=5000)
+
+  # First build the v(z) model
+  #TODO: might need to make these parameters
+  nlayer = 20
+  minvel = 1600; maxvel = 5000
+  props = mb.vofz(nlayer,minvel,maxvel)
+
+  # Specify the thicknesses
+  thicks = np.random.randint(40,61,nlayer)
+
+  # Determine when to fold the deposits
+  sqlyrs = sorted(mb.findsqlyrs(3,nlayer,5))
+  csq = 0
+
+  dlyr = 0.1
+  for ilyr in progressbar(range(nlayer), "ndeposit:", 40):
+    mb.deposit(velval=props[ilyr],thick=thicks[ilyr],band2=0.01,band3=0.05,dev_pos=0.0,layer=150,layer_rand=0.00,dev_layer=dlyr)
+    # Random folding
+    if(ilyr in sqlyrs):
+      if(sqlyrs[csq] < 15):
+        # Random amplitude variation in the folding
+        amp = np.random.rand()*(3000-500) + 500
+        mb.squish(amp=amp,azim=90.0,lam=0.4,rinline=0.0,rxline=0.0,mode='perlin')
+      elif(sqlyrs[csq] >= 15 and sqlyrs[csq] < 18):
+        amp = np.random.rand()*(1800-500) + 500
+        mb.squish(amp=amp,azim=90.0,lam=0.4,rinline=0.0,rxline=0.0,mode='perlin')
+      else:
+        amp = np.random.rand()*(500-300) + 300
+        mb.squish(amp=amp,azim=90.0,lam=0.4,rinline=0.0,rxline=0.0,mode='perlin')
+      csq += 1
+
+  # Water deposit
+  mb.deposit(1480,thick=50,layer=150,dev_layer=0.0)
+  # Trim model before faulting
+  mb.trim(0,1100)
+
+  # Fault it up!
+  azims = [0.0,180.0]
+
+  # Large faults
+  nlf = np.random.randint(2,5)
+  for ifl in progressbar(range(nlf), "nlfaults:", 40):
+    azim = np.random.choice(azims)
+    xpos = rndut.randfloat(0.1,0.9)
+    mb.largefault(azim=azim,begz=0.65,begx=xpos,begy=0.5,tscale=6.0)
+
+  # Medium faults
+  nmf = np.random.randint(3,6)
+  for ifl in progressbar(range(nmf), "nmfaults:", 40):
+    azim = np.random.choice(azims)
+    xpos = rndut.randfloat(0.05,0.95)
+    mb.mediumfault(azim=azim,begz=0.65,begx=xpos,begy=0.5,tscale=3.0)
+
+  # Small faults (sliding or small)
+  nsf = np.random.randint(5,10)
+  for ifl in progressbar(range(nsf), "nsfaults:", 40):
+    azim = np.random.choice(azims)
+    xpos = rndut.randfloat(0.05,0.95)
+    zpos = rndut.randfloat(0.2,0.5)
+    mb.smallfault(azim=azim,begz=zpos,begx=xpos,begy=0.5,tscale=2.0)
+
+  # Tiny faults
+  ntf = np.random.randint(5,10)
+  for ifl in progressbar(range(ntf), "ntfaults:", 40):
+    azim = np.random.choice(azims)
+    xpos = rndut.randfloat(0.05,0.95)
+    zpos = rndut.randfloat(0.15,0.3)
+    mb.tinyfault(azim=azim,begz=zpos,begx=xpos,begy=0.5,tscale=2.0)
+
+  # Get model
+  if(nxo == nx and nzo == nz):
+    vels[:,:,imodel] = gaussian_filter(mb.vel[slcy,:,:nz].T,sigma=args.rect).astype('float32')
+    lbls[:,:,imodel] = mb.get_label()[slcy,:,:nz].T
+    refs[:,:,imodel] = mb.get_refl()[slcy,:,:nz].T
+    # Create normalized image
+    f = rndut.randfloat(minf,maxf)
+    wav = ricker(nt,dt,f,amp,dly)
+    img = dlut.normalize(np.array([np.convolve(refs[:,ix,imodel],wav) for ix in range(nx)])[:,ns:1000+ns].T)
+    # Create noise
+    nze = dlut.normalize(bandpass(np.random.rand(nz,nx)*2-1, 2.0, 0.01, 2, pxd=43))/rndut.randfloat(3,5)
+    imgs[:,:,imodel] = img + nze
+  else:
+    velsm = gaussian_filter(mb.vel[slcy,:,:nz],sigma=0.5)
+    vels[:,:,imodel] = dlut.resample(velsm,[nxo,nzo],kind='linear').astype('float32').T
+    lbls[:,:,imodel] = dlut.thresh(dlut.resample(mb.get_label()[slcy,:,:nz],[nxo,nzo],kind='linear'),0.0).T
+    ref = mb.get_refl()[slcy,:,:nz]
+    refs[:,:,imodel] = dlut.resample(ref,[nxo,nzo],kind='linear').T
+    # Create normalized image
+    f = rndut.randfloat(minf,maxf)
+    wav = ricker(nt,dt,f,amp,dly)
+    img = np.array([np.convolve(ref[ix,:],wav) for ix in range(nx)])[:,ns:1000+ns]
+    imgo = dlut.normalize(dlut.resample(img,[nxo,nzo],kind='linear')).T
+    # Create noise
+    nze = dlut.normalize(bandpass(np.random.rand(nzo,nxo)*2-1, 2.0, 0.01, 2, pxd=43))/rndut.randfloat(3,5)
+    imgs[:,:,imodel] = imgo + nze
+
+# Write the velocity model and the label
+maxes = seppy.axes([nz,nx,nmodels],[0.0,0.0,0.0],[dz,dx,1.0])
+sep.write_file(None,maxes,vels,ofname=outmod,dpath=args.datapath)
+sep.write_file(None,maxes,lbls,ofname=outlbl,dpath=args.datapath)
+sep.write_file(None,maxes,refs,ofname=outref,dpath=args.datapath)
+sep.write_file(None,maxes,imgs,ofname=outimg,dpath=args.datapath)
+
+# Flag for cluster manager to determine success
+print("Success!")
+
