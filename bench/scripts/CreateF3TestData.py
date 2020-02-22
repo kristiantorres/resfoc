145db18b57767d024759c7c348236794b9b759d9
ke0m <joseph.s.jennings@gmail.com>
2020-02-22 15:12:49 -0800

    Now has a grid qc option

--- bench/scripts/CreateF3TestData.py
+++ bench/scripts/CreateF3TestData.py
@@ -31,6 +31,7 @@ defaults = {
     "norm": 'y',
     "wrtcube": 'y',
     "fs": 0,
+    "gridqc": 'y',
     }
 if args.conf_file:
   config = configparser.ConfigParser()
@@ -47,8 +48,8 @@ parser.set_defaults(**defaults)
 
 # Input files
 ioArgs = parser.add_argument_group('Inputs and outputs')
-ioArgs.add_argument("in=",help="Input segyfile",type=str)
-ioArgs.add_argument("out=",help="Output H5 file",type=str)
+ioArgs.add_argument("-sgyin",help="Input segyfile",type=str)
+ioArgs.add_argument("-h5out",help="Output H5 file",type=str)
 # Optional arguments
 parser.add_argument("-fs",help="First time sample from which to window the data [0]",type=int)
 parser.add_argument("-ny",help="Y dimension of output 3D cube [500]",type=int)
@@ -60,6 +61,7 @@ parser.add_argument("-strdz",help="Patch stride (overlap) in z  [64]",type=int)
 parser.add_argument("-strdx",help="Patch stride (overlap) in x [64]",type=int)
 parser.add_argument("-norm",help="Normalize each patch from -1 to 1 [y]",type=str)
 parser.add_argument("-wrtcube",help="Write out the cube built from the segy [y]",type=str)
+parser.add_argument("-gridqc",help="Plot patch grid on top of image [y]",type=str)
 parser.add_argument("-verb",help="Verbosity flag (y or [n])",type=str)
 args = parser.parse_args(remaining_argv)
 
@@ -69,13 +71,15 @@ sep = seppy.sep(sys.argv)
 # Get command line arguments
 verb  = sep.yn2zoo(args.verb)
 wrtcub = sep.yn2zoo(args.wrtcube)
+gridqc  = sep.yn2zoo(args.gridqc)
 
 # Read in the segy
-with segyio.open(sep.get_fname("in"),ignore_geometry=True) as f: 
+with segyio.open(args.sgyin,ignore_geometry=True) as f: 
   data = f.trace.raw[:]
   dt = f.attributes(segyio.TraceField.TRACE_SAMPLE_INTERVAL)[0][0]/1e6
-  dx = (f.attributes(segyio.TraceField.CDP_X)[1][0] - f.attributes(segyio.TraceField.CDP_X)[0][0])/1000.0
-  dy = (f.attributes(segyio.TraceField.CDP_Y)[1][0] - f.attributes(segyio.TraceField.CDP_Y)[0][0])/1000.0
+  dx = 25; dy = 25
+  #dx = (f.attributes(segyio.TraceField.CDP_X)[1][0] - f.attributes(segyio.TraceField.CDP_X)[0][0])/1000.0
+  #dy = (f.attributes(segyio.TraceField.CDP_Y)[1][0] - f.attributes(segyio.TraceField.CDP_Y)[0][0])/1000.0
 
 # Reshape the data so it is a regular cube
 fs = args.fs
@@ -91,7 +95,7 @@ pstride = (args.strdz,args.strdx)
 pe = PatchExtractor(pshape,stride=pstride)
 
 # Open the H5 file
-hf = h5py.File(sep.get_fname("out"),'w')
+hf = h5py.File(args.h5out,'w')
 
 # Extract the desired slices, interpolate and patch
 nxo = args.nxo; nto = args.nto
@@ -99,6 +103,21 @@ dout = np.zeros([nto,nxo,ny])
 k = 0
 for iy in progressbar(range(ny), "xline"):
   dout[:,:,k] = (dlut.resample(datawind[iy,:,:],[nxo,nto],kind='cubic')).T
+  # Plot grid on top of interpolated image
+  if(gridqc):
+    fsize = 18
+    xticks = np.arange(0,(nxo+1)*dx/1000.0,nxp*dx/1000.0)
+    yticks = np.arange(0,(nto+1)*dt,nzp*dt)
+    fig = plt.figure(1,figsize=(10,7)); ax = fig.gca()
+    plt.imshow(dout[:,:,k],cmap='gray',extent=[0.0,(nxo-1)*dx/1000.0,(nto-1)*dt,0.0],vmin=-7000,vmax=7000,interpolation='sinc')
+    plt.rc('grid', linestyle="-", color='black')
+    ax.set_xticks(xticks); ax.set_yticks(yticks)
+    ax.set_xlabel('Xline (km)',fontsize=fsize)
+    ax.set_ylabel('Time (s)',fontsize=fsize)
+    ax.tick_params(labelsize=fsize)
+    ax.set_aspect(6.5)
+    plt.grid()
+    plt.show()
   # Extract the patches
   iptch = pe.extract(dout[:,:,k])
   # Flatten
