177665245bec1f95301f2f062d3eb0d0ea2a0833
ke0m <joseph.s.jennings@gmail.com>
2020-03-15 16:03:47 -0700

    Added Huy's 3D interactive plotter

--- utils/movie.py
+++ utils/movie.py
@@ -317,3 +317,349 @@ def viewpltframeskey(data,ox=0.0,dx=1.0,transp=True,show=True,**kwargs):
 def viewframessld(data,transp=True,**kwargs):
   pass
 
+def viewcube3d(data,os=[0.0,0.0,0.0],ds=[1.0,1.0,1.0],**kwargs):
+  """
+  Plots three frames of a 3D plot for visualization. Allows
+  for user interaction with the arrow keys or e,w,n,s keys.
+  Originally written by Huy Le with some of my modifications
+
+  Parameters:
+    data - input data cube (numpy array)
+    os   - origins of each axis [0.0,0.0,0.0]
+    ds   - samplings of each axis [1.0,1.0,1.0]
+  """
+  # Transpose if requested
+  if(not kwargs.get('transp',False)):
+    data = np.expand_dims(data,axis=0)
+    data = np.transpose(data,(0,1,3,2))
+  else:
+    data = (np.expand_dims(data,axis=-1)).T
+    data = np.transpose(data,(0,1,3,2))
+  # Get the shape of the cube
+  ns = np.flip(data.shape)
+  # Make the coordinates for the cross hairs
+  ds = np.append(np.flip(ds),1.0)
+  os = np.append(np.flip(os),0.0)
+  x1=np.linspace(os[0], os[0] + ds[0]*(ns[0]-1), ns[0])
+  x2=np.linspace(os[1], os[1] + ds[1]*(ns[1]-1), ns[1])
+  x3=np.linspace(os[2], os[2] + ds[2]*(ns[2]-1), ns[2])
+
+  # Compute plotting min and max
+  if(kwargs.get('vmin',None) == None or kwargs.get('vmax',None) == None):
+    vmin = np.min(data)*kwargs.get('pclip',0.9)
+    vmax = np.max(data)*kwargs.get('pclip',0.9)
+
+  # Define nonlocal variables
+  loc1 = kwargs.get('loc1',int(ns[0]/2*ds[0]+os[0]))
+  i1 = int((loc1 - os[0])/ds[0])
+  loc2 = kwargs.get('loc2',int(ns[1]/2*ds[1]+os[1]))
+  i2 = int((loc2 - os[1])/ds[1])
+  loc3 = kwargs.get('loc3',int(ns[2]/2*ds[2]+os[2]))
+  i3 = int((loc3 - os[2])/ds[2])
+  ax1 = None; ax2 = None; ax3 = None; ax4 = None
+  curr_pos = 0
+  # Govern the keyboard movement
+  j1 = kwargs.get('j1',1); j2 = kwargs.get('j2',1); j3 = kwargs.get('j3',1)
+
+  # Axis labels
+  label1 = kwargs.get('label1',' '); label2 = kwargs.get('label2',' '); label3 = kwargs.get('label3', ' ')
+
+  def key_event(e):
+    nonlocal i1,loc1,i2,loc2,i3,loc3,ax1,ax2,ax3,ax4,curr_pos
+
+    if e.key=="u" or e.key=="d":
+      if e.key=="u":
+        i3-=j3
+      elif e.key=="d":
+        i3+=j3
+      i3=i3%ns[2]
+      loc3=i3*ds[2]+os[2]
+
+      ax[0,0].cla()
+
+      ax[0,0].imshow(np.flip(data[curr_pos,i3,:,:],0),interpolation=kwargs.get('interp','none'),aspect='auto',
+          extent=[os[0],os[0]+(ns[0])*ds[0],os[1],os[1]+(ns[1])*ds[1]],vmin=vmin,vmax=vmax,cmap=kwargs.get('cmap','gray'))
+      ax[0,0].set_ylabel(label2,fontsize=kwargs.get('labelsize',14))
+      ax[0,0].tick_params(labelsize=kwargs.get('ticksize',14))
+      del ax[0,0].lines[:]
+      ax[0,0].plot(loc1*np.ones((ns[1],)),x2,c='k')
+      ax[0,0].plot(x1,loc2*np.ones((ns[0],)),c='k')
+
+      del ax[1,0].lines[:]
+      ax[1,0].plot(loc1*np.ones((ns[2],)),x3,c='k')
+      ax[1,0].plot(x1,loc3*np.ones((ns[0],)),c='k')
+
+      del ax[1,1].lines[:]
+      ax[1,1].plot(loc2*np.ones((ns[2],)),x3,c='k')
+      ax[1,1].plot(x2,loc3*np.ones((ns[1],)),c='k')
+      ax1.set_yticks([loc3])
+      ax1.set_yticklabels(['%.2f'%(loc3)],rotation='vertical',va='center')
+      ax1.tick_params(labelsize=kwargs.get('ticksize',14))
+      ax2.set_xticks([loc2])
+      ax2.set_xticklabels(['%.2f'%(loc2)])
+      ax2.tick_params(labelsize=kwargs.get('ticksize',14))
+
+    elif(e.key=="e" or e.key=="w"):
+      if(e.key=="w"):
+        i1-=j1
+      elif(e.key=="e"):
+        i1+=j1
+      i1=i1%ns[0]
+      loc1=i1*ds[0]+os[0]
+
+      ax[1,1].cla()
+
+      ax[1,1].imshow(data[curr_pos,:,:,i1],interpolation=kwargs.get('interp','none'),aspect='auto',
+          extent=[os[1],os[1]+(ns[1])*ds[1],os[2]+(ns[2])*ds[2],os[2]],vmin=vmin,vmax=vmax,cmap=kwargs.get('cmap','gray'))
+      ax[1,1].set_xlabel(label2,fontsize=kwargs.get('labelsize',14))
+      ax[1,1].tick_params(labelsize=kwargs.get('ticksize',14))
+      del ax[1,1].lines[:]
+      ax[1,1].plot(loc2*np.ones((ns[2],)),x3,c='k')
+      ax[1,1].plot(x2,loc3*np.ones((ns[1],)),c='k')
+      ax1.set_yticks([loc3])
+      ax1.set_yticklabels(['%.2f'%(loc3)],rotation='vertical',va='center')
+      ax1.tick_params(labelsize=kwargs.get('ticksize',14))
+      ax2.set_xticks([loc2])
+      ax2.set_xticklabels(['%.2f'%(loc2)])
+      ax2.tick_params(labelsize=kwargs.get('ticksize',14))
+
+      del ax[1,0].lines[:]
+      ax[1,0].plot(loc1*np.ones((ns[2],)),x3,c='k')
+      ax[1,0].plot(x1,loc3*np.ones((ns[0],)),c='k')
+
+      del ax[0,0].lines[:]
+      ax[0,0].plot(loc1*np.ones((ns[1],)),x2,c='k')
+      ax[0,0].plot(x1,loc2*np.ones((ns[0],)),c='k')
+      ax3.set_yticks([loc2])
+      ax3.set_yticklabels(['%.2f'%(loc2)],rotation='vertical',va='center')
+      ax3.tick_params(labelsize=kwargs.get('ticksize',14))
+      ax4.set_xticks([loc1])
+      ax4.set_xticklabels(['%.2f'%(loc1)])
+      ax4.tick_params(labelsize=kwargs.get('ticksize',14))
+
+    elif e.key=="h" or e.key=="n":
+      if e.key=="h":
+        i2-=j2
+      elif e.key=="n":
+        i2+=j2
+      i2=i2%ns[1]
+      loc2=i2*ds[1]+os[1]
+
+      ax[1,0].cla()
+
+      ax[1,0].imshow(data[curr_pos,:,i2,:],interpolation=kwargs.get('interp','none'),aspect='auto',
+          extent=[os[0],os[0]+(ns[0])*ds[0],os[2]+ds[2]*(ns[2]),os[2]],vmin=vmin,vmax=vmax,cmap=kwargs.get('cmap','gray'))
+      ax[1,0].tick_params(labelsize=kwargs.get('ticksize',14))
+      del ax[1,1].lines[:]
+      ax[1,0].plot(loc1*np.ones((ns[2],)),x3,c='k')
+      ax[1,0].plot(x1,loc3*np.ones((ns[0],)),c='k')
+      ax[1,0].set_xlabel(label1,fontsize=kwargs.get('labelsize',14))
+      ax[1,0].set_ylabel(label3,fontsize=kwargs.get('labelsize',14))
+
+      del ax[1,1].lines[:]
+      ax[1,1].plot(loc2*np.ones((ns[2],)),x3,c='k')
+      ax[1,1].plot(x2,loc3*np.ones((ns[1],)),c='k')
+      ax1.set_yticks([loc3])
+      ax1.set_yticklabels(['%.2f'%(loc3)],rotation='vertical',va='center')
+      ax1.tick_params(labelsize=kwargs.get('ticksize',14))
+      ax2.set_xticks([loc2])
+      ax2.set_xticklabels(['%.2f'%(loc2)])
+      ax2.tick_params(labelsize=kwargs.get('ticksize',14))
+
+      del ax[0,0].lines[:]
+      ax[0,0].plot(loc1*np.ones((ns[1],)),x2,c='k')
+      ax[0,0].plot(x1,loc2*np.ones((ns[0],)),c='k')
+      ax3.set_yticks([loc2])
+      ax3.set_yticklabels(['%.2f'%(loc2)],rotation='vertical',va='center')
+      ax3.tick_params(labelsize=kwargs.get('ticksize',14))
+      ax4.set_xticks([loc1])
+      ax4.set_xticklabels(['%.2f'%(loc1)])
+      ax4.tick_params(labelsize=kwargs.get('ticksize',14))
+
+    elif e.key=="left" or e.key=="right" or e.key.isdigit():
+      if e.key=="left":
+        curr_pos-=1
+      if e.key=="right":
+        curr_pos+=1
+      if e.key.isdigit():
+        curr_pos=int(e.key)
+      curr_pos=curr_pos%ns[3]
+
+      ax[0,1].cla()
+      ax[0,1].get_xaxis().set_visible(False)
+      ax[0,1].get_yaxis().set_visible(False)
+      ax[0,1].axis('off')
+      ax[0,1].text(0.5,0.5,title[curr_pos],horizontalalignment='center',verticalalignment='center',fontsize=50)
+
+      ax[1,0].cla()
+      ax[1,0].imshow(data[curr_pos,:,i2,:],interpolation=kwargs.get('interp','none'),aspect='auto',
+          extent=[os[0],os[0]+(ns[0])*ds[0],os[2]+ds[2]*(ns[2]),os[2]],vmin=vmin,vmax=vmax,cmap=kwargs.get('cmap','gray'))
+      ax[1,0].plot(loc1*np.ones((ns[2],)),x3,c='k')
+      ax[1,0].plot(x1,loc3*np.ones((ns[0],)),c='k')
+      ax[1,0].set_xlabel(label1,fontsize=kwargs.get('labelsize',14))
+      ax[1,0].set_ylabel(label3,fontsize=kwargs.get('labelsize',14))
+
+      # yz plane
+      ax[1,1].cla()
+      ax[1,1].imshow(data[curr_pos,:,:,i1],interpolation=kwargs.get('interp','none'),aspect='auto',
+          extent=[os[1],os[1]+(ns[1])*ds[1],os[2]+(ns[2])*ds[2],os[2]],vmin=vmin,vmax=vmax,cmap=kwargs.get('cmap','gray'))
+      ax[1,1].plot(loc2*np.ones((ns[2],)),x3,c='k')
+      ax[1,1].plot(x2,loc3*np.ones((ns[1],)),c='k')
+      ax[1,1].set_xlabel(label2,fontsize=kwargs.get('labelsize',14))
+      ax1.set_yticks([loc3])
+      ax1.set_yticklabels(['%.2f'%(loc3)],rotation='vertical',va='center')
+      ax1.tick_params(labelsize=kwargs.get('ticksize',14))
+      ax2.set_xticks([loc2])
+      ax2.set_xticklabels(['%.2f'%(loc2)])
+      ax2.tick_params(labelsize=kwargs.get('ticksize',14))
+
+      # xy plane
+      ax[0,0].cla()
+      ax[0,0].imshow(np.flip(data[curr_pos,i3,:,:],0),interpolation=kwargs.get('interp','none'),aspect='auto',
+          extent=[os[0],os[0]+(ns[0])*ds[0],os[1],os[1]+(ns[1])*ds[1]],vmin=vmin,vmax=vmax,cmap=kwargs.get('cmap','gray'))
+      ax[0,0].plot(loc1*np.ones((ns[1],)),x2,c='k')
+      ax[0,0].plot(x1,loc2*np.ones((ns[0],)),c='k')
+      ax[0,0].set_ylabel(label2,fontsize=kwargs.get('labelsize',14))
+      ax3.set_yticks([loc2])
+      ax3.set_yticklabels(['%.2f'%(loc2)],rotation='vertical',va='center')
+      ax3.tick_params(labelsize=kwargs.get('ticksize',14))
+      ax4.set_xticks([loc1])
+      ax4.set_xticklabels(['%.2f'%(loc1)])
+      ax4.tick_params(labelsize=kwargs.get('ticksize',14))
+
+    fig.canvas.draw()
+
+  def onclick(e):
+    nonlocal i1,loc1,i2,loc2,i3,loc3,ax1,ax2,ax3,ax4,curr_pos
+    tb = plt.get_current_fig_manager().toolbar
+    if(tb.mode == ""):
+      if e.inaxes==ax1 or e.inaxes==ax2:
+        loc2=e.xdata
+        i2=int((loc2-os[1])/ds[1])
+        loc2=i2*ds[1]+os[1]
+        loc3=e.ydata
+        i3=int((loc3-os[2])/ds[2])
+        loc3=i3*ds[2]+os[2]
+      if e.inaxes==ax3 or e.inaxes==ax4:
+        loc1=e.xdata
+        i1=int((loc1-os[0])/ds[0])
+        loc1=i1*ds[0]+os[0]
+        loc2=e.ydata
+        i2=int((loc2-os[1])/ds[1])
+        loc2=i2*ds[1]+os[1]
+      if e.inaxes==ax[1,0]:
+        loc1=e.xdata
+        i1=int((loc1-os[0])/ds[0])
+        loc1=i1*ds[0]+os[0]
+        loc3=e.ydata
+        i3=int((loc3-os[2])/ds[2])
+        loc3=i3*ds[2]+os[2]
+
+      ax[1,0].cla()
+      ax[1,0].imshow(data[curr_pos,:,i2,:],interpolation=kwargs.get('interp','none'),aspect='auto',
+          extent=[os[0],os[0]+(ns[0])*ds[0],os[2]+ds[2]*(ns[2]),os[2]],vmin=vmin,vmax=vmax,cmap=kwargs.get('cmap','gray'))
+      ax[1,0].plot(loc1*np.ones((ns[2],)),x3,c='k')
+      ax[1,0].plot(x1,loc3*np.ones((ns[0],)),c='k')
+      ax[1,0].set_xlabel(label1,fontsize=kwargs.get('labelsize',14))
+      ax[1,0].set_ylabel(label3,fontsize=kwargs.get('labelsize',14))
+
+      # yz plane
+      ax[1,1].cla()
+      ax[1,1].imshow(data[curr_pos,:,:,i1],interpolation=kwargs.get('interp','none'),aspect='auto',
+          extent=[os[1],os[1]+(ns[1])*ds[1],os[2]+(ns[2])*ds[2],os[2]],vmin=vmin,vmax=vmax,cmap=kwargs.get('cmap','gray'))
+      ax[1,1].plot(loc2*np.ones((ns[2],)),x3,c='k')
+      ax[1,1].plot(x2,loc3*np.ones((ns[1],)),c='k')
+      ax[1,1].set_xlabel(label2,fontsize=kwargs.get('labelsize',14))
+      ax1.set_yticks([loc3])
+      ax1.set_yticklabels(['%.2f'%(loc3)],rotation='vertical',va='center')
+      ax1.tick_params(labelsize=kwargs.get('ticksize',14))
+      ax2.set_xticks([loc2])
+      ax2.set_xticklabels(['%.2f'%(loc2)])
+      ax2.tick_params(labelsize=kwargs.get('ticksize',14))
+
+      # xy plane
+      ax[0,0].cla()
+      ax[0,0].imshow(np.flip(data[curr_pos,i3,:,:],0),interpolation=kwargs.get('interp','none'),aspect='auto',
+          extent=[os[0],os[0]+(ns[0])*ds[0],os[1],os[1]+(ns[1])*ds[1]],vmin=vmin,vmax=vmax,cmap=kwargs.get('cmap','gray'))
+      ax[0,0].plot(loc1*np.ones((ns[1],)),x2,c='k')
+      ax[0,0].plot(x1,loc2*np.ones((ns[0],)),c='k')
+      ax[0,0].set_ylabel(label2,fontsize=kwargs.get('labelsize',14))
+      ax3.set_yticks([loc2])
+      ax3.set_yticklabels(['%.2f'%(loc2)],rotation='vertical',va='center')
+      ax4.set_xticks([loc1])
+      ax4.set_xticklabels(['%.2f'%(loc1)])
+
+      fig.canvas.draw()
+
+  width1 = kwargs.get('width1',4.0); width2 = kwargs.get('width2',4.0); width3 = kwargs.get('width3',4.0)
+  widths=[width1,width3]
+  heights=[width3,width2]
+  gs_kw=dict(width_ratios=widths,height_ratios=heights)
+  fig,ax=plt.subplots(2,2,figsize=(width1+width3,width2+width3),gridspec_kw=gs_kw)
+  plt.subplots_adjust(wspace=0,hspace=0)
+  fig.canvas.mpl_connect('key_press_event', key_event)
+  fig.canvas.mpl_connect('button_press_event', onclick)
+
+  title = kwargs.get('title',' ')
+  ax[0,1].text(0.5,0.5,title[curr_pos],horizontalalignment='center',verticalalignment='center',fontsize=50)
+
+  ## xz plane
+  ax[1,0].imshow(data[curr_pos,:,i2,:],interpolation=kwargs.get('interp','none'),aspect='auto',
+      extent=[os[0],os[0]+(ns[0])*ds[0],os[2]+ds[2]*(ns[2]),os[2]],vmin=vmin,vmax=vmax,cmap=kwargs.get('cmap','gray'))
+  ax[1,0].tick_params(labelsize=kwargs.get('ticksize',14))
+  ax[1,0].plot(loc1*np.ones((ns[2],)),x3,c='k')
+  ax[1,0].plot(x1,loc3*np.ones((ns[0],)),c='k')
+  ax[1,0].set_xlabel(label1,fontsize=kwargs.get('labelsize',14))
+  ax[1,0].set_ylabel(label3,fontsize=kwargs.get('labelsize',14))
+
+  # yz plane
+  im = ax[1,1].imshow(data[curr_pos,:,:,i1],interpolation=kwargs.get('interp','none'),aspect='auto',
+      extent=[os[1],os[1]+(ns[1])*ds[1],os[2]+(ns[2])*ds[2],os[2]],vmin=vmin,vmax=vmax,cmap=kwargs.get('cmap','gray'))
+  ax[1,1].tick_params(labelsize=kwargs.get('ticksize',14))
+  ax[1,1].plot(loc2*np.ones((ns[2],)),x3,c='k')
+  ax[1,1].plot(x2,loc3*np.ones((ns[1],)),c='k')
+  ax[1,1].get_yaxis().set_visible(False)
+  ax[1,1].set_xlabel(label2,fontsize=kwargs.get('labelsize',14))
+  ax1=ax[1,1].twinx()
+  ax1.set_ylim(ax[1,1].get_ylim())
+  ax1.set_yticks([loc3])
+  ax1.set_yticklabels(['%.2f'%(loc3)],rotation='vertical',va='center')
+  ax1.tick_params(labelsize=kwargs.get('ticksize',14))
+  ax2=ax[1,1].twiny()
+  ax2.set_xlim(ax[1,1].get_xlim())
+  ax2.set_xticks([loc2])
+  ax2.set_xticklabels(['%.2f'%(loc2)])
+  ax2.tick_params(labelsize=kwargs.get('ticksize',14))
+
+  # xy plane
+  ax[0,0].imshow(np.flip(data[curr_pos,i3,:,:],0),interpolation=kwargs.get('interp','none'),aspect='auto',
+      extent=[os[0],os[0]+(ns[0])*ds[0],os[1],os[1]+(ns[1])*ds[1]],vmin=vmin,vmax=vmax,cmap=kwargs.get('cmap','gray'))
+  ax[0,0].tick_params(labelsize=kwargs.get('ticksize',14))
+  ax[0,0].plot(loc1*np.ones((ns[1],)),x2,c='k')
+  ax[0,0].plot(x1,loc2*np.ones((ns[0],)),c='k')
+  ax[0,0].set_ylabel(label2,fontsize=kwargs.get('labelsize',14))
+  ax[0,0].get_xaxis().set_visible(False)
+  ax3=ax[0,0].twinx()
+  ax3.set_ylim(ax[0,0].get_ylim())
+  ax3.set_yticks([loc2])
+  ax3.set_yticklabels(['%.2f'%(loc2)],rotation='vertical',va='center')
+  ax3.tick_params(labelsize=kwargs.get('ticksize',14))
+  ax4=ax[0,0].twiny()
+  ax4.set_xlim(ax[0,0].get_xlim())
+  ax4.set_xticks([loc1])
+  ax4.set_xticklabels(['%.2f'%(loc1)])
+  ax4.tick_params(labelsize=kwargs.get('ticksize',14))
+
+  # Color bar
+  if(kwargs.get('cbar',False)):
+    fig.subplots_adjust(right=0.87)
+    cbar_ax = fig.add_axes([kwargs.get('barx',0.91),kwargs.get('barz',0.11),
+      kwargs.get('wbar',0.02),kwargs.get('hbar',0.78)])
+    cbar = fig.colorbar(im,cbar_ax,format='%.2f')
+    cbar.ax.tick_params(labelsize=kwargs.get('ticksize',14))
+    cbar.set_label(kwargs.get('barlabel',''),fontsize=kwargs.get("barlabelsize",13))
+    cbar.draw_all()
+
+  ax[0,1].axis('off')
+  plt.show()
+
