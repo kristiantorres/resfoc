34693a12b1c22dcd2966935644a070ab798bb291
ke0m <joseph.s.jennings@gmail.com>
2020-04-03 19:11:25 -0700

    Updated for the full large model

--- bench/image/scripts/resang.py
+++ bench/image/scripts/resang.py
@@ -13,16 +13,14 @@ img = img.reshape(iaxes.n,order='F')
 dz = iaxes.d[0]; dx = iaxes.d[1]; dh = iaxes.d[2]
 
 imgt = np.ascontiguousarray(np.transpose(img,(2,0,1))).astype('float32')
-imgtw = imgt[:,:,0:512]
 #viewimgframeskey(imgt,transp=False)
 
 print(imgt.shape)
 #viewimgframeskey(imgt,transp=False)
 
 # First do residual migration
-nps = [513,1025,1025]
-inro = 2; idro = 0.005; oro=1.0
-storm = preresmig(imgtw,[dh,dz,dx],nps=[129,1025,1025],nro=inro,dro=idro,oro=oro,time=False,transp=True,verb=True,nthreads=2)
+inro = 5; idro = 0.005; oro=1.0
+storm = preresmig(imgt,[dh,dz,dx],nps=[2049,513,1025],nro=inro,dro=idro,oro=oro,time=False,transp=True,verb=True,nthreads=1)
 onro,ooro,odro = get_rho_axis(nro=inro,dro=idro)
 
 nh = storm.shape[1]
