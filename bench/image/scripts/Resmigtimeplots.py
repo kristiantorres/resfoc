00fa5856cafba9c8fa63e99c685dbe359c1727b8
ke0m <joseph.s.jennings@gmail.com>
2020-04-07 23:40:18 -0700

    Improved plotting capabilities for seminar presentation

--- bench/image/scripts/Resmigtimeplots.py
+++ bench/image/scripts/Resmigtimeplots.py
@@ -9,12 +9,11 @@ import inpout.seppy as seppy
 from resfoc.gain import agc
 import matplotlib.pyplot as plt
 from utils.movie import makemovie_mpl, viewimgframeskey, makemovietb_mpl, makemoviesbs_mpl
-from utils.plot import plot_anggatrhos
 
 sep = seppy.sep()
 
 # Read in stack
-saxes,stk = sep.read_file("resangwrngconvstk.H")
+saxes,stk = sep.read_file("resangwrngconvtimestk.H")
 [nz,nx,nro] = saxes.n; [oz,ox,oro] = saxes.o; [dz,dx,dro] = saxes.d
 stk = stk.reshape(saxes.n,order='F')
 stk = np.ascontiguousarray(stk.T).astype('float32')
@@ -24,32 +23,32 @@ gne = agc(stk)
 #viewimgframeskey(gne[:,100:610,:],transp=True,pclip=0.5)
 #
 #bxidx = 20; exidx = 1000
-#makemovie_mpl(gne.T[:,bxidx:exidx,:],'./fig/stkrmig',qc=False,pclip=0.5,ttlstring=r'$\rho$=%.4f',ottl=oro,dttl=dro,
-#    labelsize=15,hbox=6,wbox=10,ticksize=15,xlabel='X (km)',ylabel='Z (km)',
-#    zmax=(nz-1)*dz/1000.0,xmin=bxidx*dx/1000.0,xmax=(exidx-1)*dx/1000.0)
+#makemovie_mpl(gne.T[:,bxidx:exidx,:],'./fig/stkrmigtime',qc=True,pclip=0.5,ttlstring=r'$\rho$=%.4f',ottl=oro,dttl=dro,
+#    labelsize=15,hbox=6,wbox=10,ticksize=15,xlabel='X (km)',ylabel='Time (S)',
+#    zmax=(nz-1)*dz,xmin=bxidx*dx/1000.0,xmax=(exidx-1)*dx/1000.0,aratio=2.5)
 
 # Read in angle depth gathers
-aaxes,ang = sep.read_file("resangwrngconv.H")
+aaxes,ang = sep.read_file("resangwrngconvtime.H")
 [nz,na,nx,nro] = aaxes.n; [oz,oa,ox,oro] = aaxes.o; [dz,da,dx,dro] = aaxes.d
 ang = ang.reshape(aaxes.n,order='F')
 ang = np.ascontiguousarray(ang.T).astype('float32')
 
-#jx = 10
-#angss = ang[:,::jx,:,:]
-#nxs = angss.shape[1]
-#angssr = angss.reshape([nro,na*nxs,nz])
-#
-#anggne = agc(angssr)
-#
-##viewimgframeskey(anggne,transp=True,pclip=0.5)
-#
-#ntot = anggne.shape[1]
-#minidx = 10*na; maxidx = int(ntot) - 10*na
-#ixmin = int(minidx/na)*jx; ixmax = int(maxidx/na)*jx
+jx = 10
+angss = ang[:,::jx,:,:]
+nxs = angss.shape[1]
+angssr = angss.reshape([nro,na*nxs,nz])
 
-#makemovie_mpl(anggne[:,minidx:maxidx,75:450].T,'./fig/angrmig',qc=True,pclip=0.5,ttlstring=r'$\rho$=%.4f',ottl=oro,dttl=dro,
-#    labelsize=15,hbox=6,wbox=10,ticksize=15,xlabel='X (km)',ylabel='Z (km)',zmin=(75)*dz/1000.0,
-#    zmax=(450-1)*dz/1000.0,xmin=ixmin*dx/1000.0,xmax=ixmax*dx/1000.0)
+anggne = agc(angssr)
+
+#viewimgframeskey(anggne,transp=True,pclip=0.5)
+
+ntot = anggne.shape[1]
+minidx = 10*na; maxidx = int(ntot) - 10*na
+ixmin = int(minidx/na)*jx; ixmax = int(maxidx/na)*jx
+
+makemovie_mpl(anggne[:,minidx:maxidx,75:450].T,'./fig/angrmigtime',qc=False,pclip=0.5,ttlstring=r'$\rho$=%.4f',ottl=oro,dttl=dro,
+    labelsize=15,hbox=6,wbox=10,ticksize=15,xlabel='X (km)',ylabel='Time (S)',zmin=(75)*dz,
+    zmax=(450-1)*dz,xmin=ixmin*dx/1000.0,xmax=ixmax*dx/1000.0,aratio=2.5)
 
 #minidx = 10*na; maxidx = int(ntot/2) + 10*na
 #minidx = int(ntot/2); maxidx = int(ntot) - 10*na
@@ -67,18 +66,4 @@ ang = np.ascontiguousarray(ang.T).astype('float32')
 #    labelsize=15,hbox=7,wbox=15,ticksize=15,xlabel2='X (km)',ylabel1='Z (km)',ylabel2='Z (km)',
 #    xmin1=ixmin*dx/1000.0,xmax1=ixmax*dx/1000.0,xmin2=ixmin*dx/1000.0,xmax2=ixmax*dx/1000.0,
 #    zmin1=(50)*dz/1000.0,zmax1=(400)*dz/1000.0,zmin2=(50)*dz/1000.0,zmax2=(400)*dz/1000.0,aspect1=0.5,aspect2=1.0)
-
-
-# Plot single angle gathers
-show=False
-plot_anggatrhos(ang,xloc=180,dz=dz/1000.0,dx=dx/1000.0,oro=oro,dro=dro,figname='./fig/anggat180',agc=True,
-    wboxi=14,hboxi=6,xmin=20,xmax=999,zmin=50,zmax=400,labelsize=15,show=show)
-plot_anggatrhos(ang,xloc=350,dz=dz/1000.0,dx=dx/1000.0,oro=oro,dro=dro,figname='./fig/anggat350',agc=True,
-    wboxi=14,hboxi=6,xmin=20,xmax=999,zmin=50,zmax=400,labelsize=15,show=show)
-plot_anggatrhos(ang,xloc=500,dz=dz/1000.0,dx=dx/1000.0,oro=oro,dro=dro,figname='./fig/anggat500',agc=True,
-    wboxi=14,hboxi=6,xmin=20,xmax=999,zmin=50,zmax=400,labelsize=15,show=show)
-plot_anggatrhos(ang,xloc=700,dz=dz/1000.0,dx=dx/1000.0,oro=oro,dro=dro,figname='./fig/anggat700',agc=True,
-    wboxi=14,hboxi=6,xmin=20,xmax=999,zmin=50,zmax=400,labelsize=15,show=show)
-plot_anggatrhos(ang,xloc=830,dz=dz/1000.0,dx=dx/1000.0,oro=oro,dro=dro,figname='./fig/anggat830',agc=True,
-    wboxi=14,hboxi=6,xmin=20,xmax=999,zmin=50,zmax=400,labelsize=15,show=show)
 
---
+++ bench/ml/scripts/segmentfaults_resmig.py
@@ -0,0 +1,168 @@
+"""
+Reads in an image or a cube and performs a 2D segmentation
+of each image in the cube
+
+@author: Joseph Jennings
+@version: 2020.04.05
+"""
+import sys, os, argparse, configparser
+import inpout.seppy as seppy
+import numpy as np
+from utils.ptyprint import create_inttag
+from deeplearn.python_patch_extractor.PatchExtractor import PatchExtractor
+from deeplearn.utils import plotseglabel, plotsegprobs, thresh, normalize, resample
+from tensorflow.keras.models import model_from_json
+import tensorflow as tf
+from resfoc.gain import agc,tpow
+import matplotlib.pyplot as plt
+
+# Parse the config file
+conf_parser = argparse.ArgumentParser(add_help=False)
+conf_parser.add_argument("-c", "--conf_file",
+                         help="Specify config file", metavar="FILE")
+args, remaining_argv = conf_parser.parse_known_args()
+defaults = {
+    "verb": "n",
+    "thresh": 0.5,
+    "show": 'n',
+    "gpus": [],
+    "aratio": 1.0,
+    "fs": 0,
+    "time": "n",
+    "km": "y",
+    "barx": 0.91,
+    "barz": 0.31,
+    "hbar": 0.37,
+    "exidx": 600,
+    "cropsize": 154,
+    "pmin": 0.3,
+    }
+if args.conf_file:
+  config = configparser.ConfigParser()
+  config.read([args.conf_file])
+  defaults = dict(config.items("defaults"))
+
+# Parse the other arguments
+# Don't surpress add_help here so it will handle -h
+parser = argparse.ArgumentParser(parents=[conf_parser],description=__doc__,
+    formatter_class=argparse.RawDescriptionHelpFormatter)
+
+# Set defaults
+parser.set_defaults(**defaults)
+
+# Input files
+ioArgs = parser.add_argument_group('Inputs and outputs')
+ioArgs.add_argument("-imgs",help="input images",type=str,required=True)
+ioArgs.add_argument("-wgts",help="input CNN weights",type=str,required=True)
+ioArgs.add_argument("-arch",help="input CNN architecture",type=str,required=True)
+ioArgs.add_argument("-figpfx",help="Output directory of where to save figs",type=str)
+# Required arguments
+ptchArgs = parser.add_argument_group('Patching parameters')
+ptchArgs.add_argument('-nxo',help='Output x for resampling before patching [1024]',type=int)
+ptchArgs.add_argument('-nzo',help='Output z for resampling before patching [512]',type=int)
+ptchArgs.add_argument('-ptchx',help='X dimension of patch',type=int,required=True)
+ptchArgs.add_argument('-ptchz',help='Z dimension of patch',type=int,required=True)
+ptchArgs.add_argument('-strdx',help='X dimension of stride',type=int,required=True)
+ptchArgs.add_argument('-strdz',help='Z dimension of stride',type=int,required=True)
+# Plotting arguments
+plotArgs = parser.add_argument_group('Plotting parameters')
+plotArgs.add_argument("-aratio",help="Aspect ratio for plotting",type=float)
+plotArgs.add_argument("-pmin",help="Minimum probablility to display [0.3]",type=float)
+plotArgs.add_argument("-fs",help="First sample for windowing the plots",type=int)
+plotArgs.add_argument("-bxidx",help="First X sample for windowing the plots [0]",type=int)
+plotArgs.add_argument("-exidx",help="Last X sample for windowing the plots [600]",type=int)
+plotArgs.add_argument("-time",help="Flag for a time or depth image [n]",type=str)
+plotArgs.add_argument("-km",help="Flag for plotting in meters or kilometers [y]",type=str)
+plotArgs.add_argument("-barx",help="X position of colorbar [0.91]",type=float)
+plotArgs.add_argument("-barz",help="Z position of colorbar [0.31]",type=float)
+plotArgs.add_argument("-hbar",help="Colorbar height [0.37]",type=float)
+plotArgs.add_argument("-cropsize",help="Amount to crop on the unsegmented image for removing colorbar",type=int)
+# Optional arguments
+parser.add_argument("-thresh",help="Threshold to apply to predictions [0.5]",type=float)
+parser.add_argument("-verb",help="Verbosity flag (y or [n])",type=str)
+parser.add_argument("-show",help="Show plots before saving [n]",type=str)
+parser.add_argument("-gpus",help="A comma delimited list of which GPUs to use [default all]",type=str)
+# Enables required arguments in config file
+for action in parser._actions:
+  if(action.dest in defaults):
+    action.required = False
+args = parser.parse_args(remaining_argv)
+
+# Set up SEP
+sep = seppy.sep()
+
+# Get command line arguments
+verb  = sep.yn2zoo(args.verb)
+show = False
+if(sep.yn2zoo(args.show)): show = True
+time = False
+if(sep.yn2zoo(args.time)): time = True
+km = False
+if(sep.yn2zoo(args.km)): km = True
+gpus  = sep.read_list(args.gpus,[])
+if(len(gpus) != 0):
+  for igpu in gpus: os.environ['CUDA_VISIBLE_DEVICES'] = str(igpu)
+
+# Read in the image
+iaxes,imgs = sep.read_file(args.imgs)
+imgs = imgs.reshape(iaxes.n,order='F')
+if(len(imgs.shape) < 3):
+  imgs = np.expand_dims(imgs,axis=-1)
+imgs = np.transpose(imgs,(2,0,1))
+nz = imgs.shape[1]; nx = imgs.shape[2]; nimg = imgs.shape[0]
+dz = iaxes.d[0]; dx = iaxes.d[1]
+
+# Perform the patch extraction
+nzo = args.nzo;   nxo = args.nxo
+nzp = args.ptchz; nxp = args.ptchx
+pe = PatchExtractor((nzp,nxp),stride=(args.strdx,args.strdz))
+
+## Read in the network
+with open(args.arch,'r') as f:
+  model = model_from_json(f.read())
+
+wgts = model.load_weights(args.wgts)
+
+if(verb): model.summary()
+
+# Set GPUs
+tf.compat.v1.GPUOptions(allow_growth=True)
+
+# Loop over all images
+for iimg in range(nimg):
+  # Resample the images to the output size
+  if(nimg == 1):
+    rimg,ds = resample(imgs[0,:,:],[nzo,nxo],kind='linear',ds=[dz,dx])
+  else:
+    rimg,ds = resample(imgs[iimg,:,:],[nzo,nxo],kind='linear',ds=[dz,dx])
+  # Perform the patch extraction
+  iptch = pe.extract(rimg)
+  numpz = iptch.shape[0]; numpx = iptch.shape[1]
+  iptch = iptch.reshape([numpx*numpz,nzp,nxp,1])
+  # Normalize each patch
+  niptch = np.zeros(iptch.shape)
+  for ip in range(numpz*numpx):
+    niptch[ip,:,:] = normalize(iptch[ip,:,:])
+  # Make a prediction
+  iprd  = model.predict(niptch,verbose=1)
+  # Reconstruct and plot the predictions
+  ipra  = iprd.reshape([numpz,numpx,nzp,nxp])
+  iprb  = pe.reconstruct(ipra)
+  tprb  = thresh(iprb,args.thresh)
+  # Plot the prediction and the image
+  if(not time and km):
+    ds[0] /= 1000; ds[1] /= 1000
+  elif(time and km):
+    ds[1] /= 1000
+  gimg = agc(rimg.astype('float32').T).T
+  # Get window
+  fs = args.fs
+  bxidx = args.bxidx; exidx = args.exidx
+  # Make plot
+  oro = 0.9775; dro = 0.0025
+  plotsegprobs(gimg[fs:,bxidx:exidx],iprb[fs:,bxidx:exidx],oro+iimg*dro,
+             xlabel='X (km)',ylabel='Z (km)',xmin=bxidx*ds[1],xmax=(exidx-1)*ds[1],
+             zmin=fs*ds[0],zmax=(nz-1)*ds[0],vmin=-2.5,vmax=2.5,aratio=args.aratio,show=show,interp='sinc',
+             pmin=args.pmin,alpha=0.7,fname=str(iimg),ticksize=14,barlabelsize=14,barx=args.barx,
+             hbar=args.hbar,wbox=10,labelsize=14,barz=args.barz,cropsize=args.cropsize)
+
