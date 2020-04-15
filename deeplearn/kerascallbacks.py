fd1bc5d2b83fd46ce48ba80492dd87b93d98f4e1
ke0m <joseph.s.jennings@gmail.com>
2020-04-14 23:57:30 -0700

    Refocusing with target

--- deeplearn/kerascallbacks.py
+++ deeplearn/kerascallbacks.py
@@ -10,6 +10,7 @@ from tensorflow.keras import callbacks
 from deeplearn.dataloader import load_allflddata
 from deeplearn.python_patch_extractor.PatchExtractor import PatchExtractor
 from deeplearn.utils import thresh, plotseglabel
+from resfoc.estro import estro_tgt, onehot2rho
 from utils.ptyprint import create_inttag
 import numpy as np
 import matplotlib.pyplot as plt
@@ -54,4 +55,52 @@ class F3Pred(callbacks.Callback):
           xlabel='Inline',ylabel='Time (s)',xmin=0.0,xmax=(nx-1)*25/1000.0,
           zmin=(self.fs-1)*0.004,zmax=(nt-1)*0.004,vmin=-2.5,vmax=2.5,interp='sinc',aratio=6.5)
       plt.savefig(self.figpath + '/ep%s.png'%(create_inttag(epoch,100)),bbox_inches='tight',dpi=150)
+
+
+class ShowPred(callbacks.Callback):
+
+  def __init__(self,fpath,rpath,ppath,psize=(19,128,128),ssize=(19,64,64),thresh=0.5,predpath=None,figpath=None):
+    # Initialize SEPlib
+    sep = seppy.sep()
+    # Residual migration image
+    raxes,res = sep.read_file(rpath)
+    res = res.reshape(raxes.n,order='F')
+    #rzro = res[:,:,16,:]
+    rzro = res[psize[1]:,:,16,:]
+    [nz,nx,nh,self.nro] = raxes.n; [dz,dx,dh,self.dro] = raxes.d; [ox,ox,oh,self.oro] = raxes.o
+    # Perturbation
+    paxes,ptb = sep.read_file(ppath)
+    self.ptb = ptb.reshape(paxes.n,order='F')
+    self.ptb = self.ptb[psize[1]:,:]
+    # Well focused image
+    iaxes,img = sep.read_file(fpath)
+    img = img.reshape(iaxes.n,order='F')
+    #izro = img[:,:,16]
+    izro = img[psize[1]:,:,16]
+    # Patch it
+    self.pe = PatchExtractor(psize,stride=ssize)
+    rzrop = np.squeeze(self.pe.extract(rzro.T))
+    self.px = rzrop.shape[0]; self.pz = rzrop.shape[1]
+    rzrop = rzrop.reshape([self.px*self.pz,self.nro,psize[1],psize[2]])
+    self.rzropt = np.transpose(rzrop,(0,2,3,1))
+    # Compute ground truth
+    self.rho = estro_tgt(rzro.T,izro.T,self.dro,self.oro,nzp=psize[2],nxp=psize[1],strdx=ssize[1],strdz=ssize[2])
+    # Save values
+    self.thresh = thresh
+
+  def on_epoch_end(self,epoch,logs={}):
+    # Make a prediction on the input image
+    print("Predicting on test image...")
+    pred = self.model.predict(self.rzropt,verbose=1)
+    print(pred)
+    # Apply threshold
+    #tpred = thresh(pred,self.thresh)
+    pred = pred.reshape([self.px,self.pz,self.nro])
+    prho = onehot2rho(pred,self.dro,self.oro,nz=512-128)
+    #prho = onehot2rho(tpred,self.dro,self.oro,nz=512)
+    fig,ax = plt.subplots(1,3,figsize=(14,7))
+    ax[0].imshow(prho.T,cmap='seismic',vmin=0.97,vmax=1.03)
+    ax[1].imshow(self.rho.T,cmap='seismic',vmin=0.97,vmax=1.03)
+    ax[2].imshow(self.ptb,cmap='jet',vmin=-100,vmax=100)
+    plt.show()
 
