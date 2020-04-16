f7fcfd9a528e72c082a4317a3e9f12c194c09dbb
ke0m <joseph.s.jennings@gmail.com>
2020-04-15 22:38:50 -0700

    Updates to some scripts

---
+++ bench/scripts/create_ssimfoctrdata.py
@@ -0,0 +1,153 @@
+"""
+Creates patched data in H5 format from SEPlib files.
+The output is a H5 file with images and labels that
+is ready to be read in for deep learning
+
+@author: Joseph Jennings
+@version: 2020.02.10
+"""
+import sys, os, argparse, configparser
+import inpout.seppy as seppy
+import h5py
+import glob
+from utils.ptyprint import progressbar, create_inttag
+import numpy as np
+import deeplearn.utils as dlut
+from resfoc.estro import estro_tgt
+from deeplearn.python_patch_extractor.PatchExtractor import PatchExtractor
+import matplotlib.pyplot as plt
+
+# Parse the config file
+conf_parser = argparse.ArgumentParser(add_help=False)
+conf_parser.add_argument("-c", "--conf_file",
+                         help="Specify config file", metavar="FILE")
+args, remaining_argv = conf_parser.parse_known_args()
+defaults = {
+    "sepdir": None,
+    "verb": "y",
+    "filelist": "n",
+    "prefix": "",
+    "norm": 'y',
+    "ptchz": 128,
+    "ptchx": 128,
+    "strdx": 64,
+    "strdz": 64,
+    "nfiles": 210,
+    "blacklist": ['00760'],
+    }
+if args.conf_file:
+  config = configparser.SafeConfigParser()
+  config.read([args.conf_file])
+  defaults = dict(config.items("defaults"))
+# Parse the other arguments
+# Don't surpress add_help here so it will handle -h
+parser = argparse.ArgumentParser(parents=[conf_parser],description=__doc__,
+    formatter_class=argparse.RawDescriptionHelpFormatter)
+
+# Set defaults
+parser.set_defaults(**defaults)
+
+# Input files
+parser.add_argument("-imgdir",help="Directory containing focused image SEPlib files",type=str,required=True)
+parser.add_argument("-resdir",help="Directory containing residual migration SEPlib files",type=str,required=True)
+parser.add_argument("-ptbdir",help="Directory containing velocity perturbation SEPlib files",type=str,required=True)
+parser.add_argument("-out",help="output h5 file",type=str,required=True)
+parser.add_argument("-resprefix",help="Prefix to residual migration files [resfltimg]",type=str,required=True)
+parser.add_argument("-ptbprefix",help="Prefix to velocity perturbation files [resfltptb]",type=str,required=True)
+parser.add_argument("-imgprefix",help="Prefix to migration files [fltimg]",type=str,required=True)
+# Optional arguments
+parser.add_argument("-filelist",help="Path to a file list. If one is not provided, one will be created [None]",type=str)
+parser.add_argument("-verb",help="Verbosity flag ([y] or n)",type=str)
+parser.add_argument("-ptchz",help="Size of patch in z [128]",type=int)
+parser.add_argument("-ptchx",help="Size of patch in x [128]",type=int)
+parser.add_argument("-strdz",help="Patch stride (overlap) in z  [64]",type=int)
+parser.add_argument("-strdx",help="Patch stride (overlap) in x [64]",type=int)
+parser.add_argument("-norm",help="Normalize each patch from -1 to 1 [y]",type=str)
+parser.add_argument("-nfiles",help="Number of SEPlib files to use [210]",type=int)
+# Enables required arguments in config file
+for action in parser._actions:
+  if(action.dest in defaults):
+    action.required = False
+args = parser.parse_args(remaining_argv)
+
+# Set up IO
+sep = seppy.sep()
+
+# Get command line arguments
+out = args.out
+imgprefix = args.imgprefix
+resprefix = args.resprefix
+ptbprefix = args.ptbprefix
+nfiles = args.nfiles
+
+norm = sep.yn2zoo(args.norm)
+verb = sep.yn2zoo(args.verb)
+
+# Get SEPlib files
+resfiles = sorted(glob.glob(args.resdir + '/' + resprefix + '-*.H'))
+imgfiles = sorted(glob.glob(args.imgdir + '/' + imgprefix + '-*.H'))
+ptbfiles = sorted(glob.glob(args.ptbdir + '/' + ptbprefix + '-*.H'))
+
+resmtch = []; ptbmtch = []; imgmtch = []
+for iimg in imgfiles:
+  # Get the number of the file
+  inum = iimg.split(imgprefix+'-')[-1]
+  ires = args.resdir + '/' + resprefix + '-' + inum
+  iptb = args.ptbdir + '/' + ptbprefix + '-' + inum
+  # Append to lists
+  if(ires in resfiles and iptb in ptbfiles):
+    imgmtch.append(iimg); resmtch.append(ires); ptbmtch.append(iptb)
+
+if(verb): print("Total number of files: %d"%len(imgmtch))
+
+# Window to the appropriate number of files
+imgwind = imgmtch[:nfiles]
+reswind = resmtch[:nfiles]
+ptbwind = ptbmtch[:nfiles]
+
+# Get the rho axis
+raxes,_ = sep.read_file(resmtch[0])
+[nz,nx,nh,nro] = raxes.n; [dz,dx,dh,dro] = raxes.d; [oz,ox,oh,oro] = raxes.o
+
+# Build the patch extractor
+nzp = args.ptchz; nxp = args.ptchx
+pshape = (nro,nxp,nzp)
+strdx = args.strdx; strdz = args.strdz
+pstride = (nro,strdx,strdz)
+pe = PatchExtractor(pshape,stride=pstride)
+
+# Get pointer to H5 file
+hf = h5py.File(out,'w')
+
+k = 0
+# Loop over the files
+for ifile in progressbar(range(nfiles), "nfiles"):
+  # Read in the files
+  iaxes,img = sep.read_file(imgwind[ifile])
+  img = img.reshape(iaxes.n,order='F')
+  raxes,res = sep.read_file(reswind[ifile])
+  res = res.reshape(raxes.n,order='F')
+  paxes,ptb = sep.read_file(ptbwind[ifile])
+  ptb = ptb.reshape(paxes.n,order='F')
+  #  Window the data and get the rho axis
+  #izro = img[nzp:,:,16]; rzro = res[nzp:,:,16,:]; pzro = ptb[nzp:,:]
+  izro = img[:,:,16]; rzro = res[:,:,16,:]; pzro = ptb[:,:]
+  # Compute the label
+  rho,lbls = estro_tgt(rzro.T,izro.T,dro,oro,nzp=nzp,nxp=nxp,strdx=strdx,strdz=strdz,onehot=True)
+  fig,ax = plt.subplots(1,2,figsize=(14,7))
+  ax[0].imshow(rho.T,cmap='seismic',vmin=0.98,vmax=1.02)
+  ax[1].imshow(pzro,cmap='jet',vmin=-100,vmax=100)
+  plt.show()
+  # Create image patches
+  rzrop = np.squeeze(pe.extract(rzro.T))
+  # Flatten the labels and write to H5 file
+  px = lbls.shape[0]; pz = lbls.shape[1]
+  lbls = lbls.reshape([px*pz,nro]); rzrop = rzrop.reshape([px*pz,nro,nxp,nzp])
+  rzropt = np.transpose(rzrop,(0,2,3,1))
+  datatag = create_inttag(k,nfiles)
+  hf.create_dataset("x"+datatag, (pz*px,nxp,nzp,nro), data=rzropt, dtype=np.float32)
+  hf.create_dataset("y"+datatag, (pz*px,nro), data=lbls, dtype=np.float32)
+  #TODO: save the velocity perturbation files
+  k += 1
+
+hf.close()
--- bench/scripts/make_vel_figs.py
+++ bench/scripts/make_vel_figs.py
@@ -6,67 +6,82 @@ from deeplearn.utils import plotseglabel, resample
 from utils.ptyprint import progressbar, create_inttag
 
 # Initialize IO
-sep = seppy.sep([])
+sep = seppy.sep()
 
 # Get all of the models
-mdls = sorted(glob.glob("./dat/velflts/velfltmod*.H"))[0:2]
-imgs = sorted(glob.glob("./dat/velflts/velfltimg*.H"))[0:2]
-lbls = sorted(glob.glob("./dat/velflts/velfltlbl*.H"))[0:2]
+mdls = sorted(glob.glob("./dat/velflts/velfltmod*.H"))[0:20]
+imgs = sorted(glob.glob("./dat/velflts/velfltimg*.H"))[0:20]
+lbls = sorted(glob.glob("./dat/velflts/velfltlbl*.H"))[0:20]
+refs = sorted(glob.glob("./dat/velflts/velfltref*.H"))[0:20]
 
-fsize = 18
+fsize = 14
 # Loop over all models and create the figures
 for ifl in progressbar(range(len(mdls)), "nexamples"):
   # Get the number of the model
   num = int(mdls[ifl].split('velfltmod')[-1].split('.')[0])
   # Read in the velocity model
-  vaxes,vel = sep.read_file(None,ifname=mdls[ifl])
+  vaxes,vel = sep.read_file(ifname=mdls[ifl])
   vel = vel.reshape(vaxes.n,order='F')
   # Read in the image
-  iaxes,img = sep.read_file(None,ifname=imgs[ifl])
+  iaxes,img = sep.read_file(ifname=imgs[ifl])
   img = img.reshape(iaxes.n,order='F')
   # Read in the label
-  laxes,lbl = sep.read_file(None,ifname=lbls[ifl])
+  laxes,lbl = sep.read_file(ifname=lbls[ifl])
   lbl = lbl.reshape(laxes.n,order='F')
-  # Get the axes
-  nz = vaxes.n[0]; dz = 5/1000.0#vaxes.d[0]/1000.0
-  nx = vaxes.n[1]; dx = 10/1000.0#vaxes.d[1]/1000.0
-  # Get the number of examples in the file
+  # Read in the reflectivity
+  raxes,ref = sep.read_file(ifname=refs[ifl])
+  ref = ref.reshape(raxes.n,order='F')
   if(len(vaxes.n) == 3):
-    nex = vaxes.n[2]
+    # Get the axes
+    [nz,nx,nex] = vaxes.n
   else:
-    nex = 1
+    # Get the axes
+    [nz,nx] = vaxes.n; nex = 1
     vel = np.expand_dims(vel,axis=-1)
+    ref = np.expand_dims(ref,axis=-1)
     img = np.expand_dims(img,axis=-1)
     lbl = np.expand_dims(lbl,axis=-1)
+  dz = 0.005; dx = 0.010;
+  # Get the number of examples in the file
   for iex in range(nex):
     # Plot the velocity model
-    fig1 = plt.figure(1,figsize=(14,7)); ax1 = fig1.gca()
+    fig1 = plt.figure(1,figsize=(8,6)); ax1 = fig1.gca()
     im1 = ax1.imshow(vel[:,:,iex]/1000.0,cmap='jet',interpolation='bilinear',vmin=1.5,vmax=4.5,
-              extent=[0.0,(nx-1)*dx,(nz-1)*dz,0.0])
+              extent=[0.0,(nx)*dx,(nz)*dz,0.0])
     ax1.set_xlabel('X (km)',fontsize=fsize)
     ax1.set_ylabel('Z (km)',fontsize=fsize)
     ax1.tick_params(labelsize=fsize)
     # Colorbar
-    cbar_ax = fig1.add_axes([0.91,0.11,0.02,0.77])
-    cbar = fig1.colorbar(im1,cbar_ax,format='%.2f')
+    cbar_ax = fig1.add_axes([0.92,0.24,0.02,0.51])
+    cbar = fig1.colorbar(im1,cbar_ax,format='%.1f')
     cbar.ax.tick_params(labelsize=fsize)
     cbar.set_label('velocity (km/s)',fontsize=fsize)
     cbar.draw_all()
-    plt.savefig('./fig/velflts/vels/vel'+create_inttag(num+iex,10000)+'.png',bbox_inches='tight',dpi=150)
+    #plt.show()
+    plt.savefig('./fig/velflts/velsnew/vel'+create_inttag(num+iex,10000)+'.png',bbox_inches='tight',dpi=150,transparent=True)
     plt.close()
     # Plot the image
-    fig2 = plt.figure(2,figsize=(14,7)); ax2 = fig2.gca()
+    fig2 = plt.figure(2,figsize=(8,6)); ax2 = fig2.gca()
     im2 = ax2.imshow(img[:,:,iex],cmap='gray',interpolation='sinc',vmin=-3,vmax=3,
-        extent=[0.0,(nx-1)*dx,(nz-1)*dz,0.0])
+        extent=[0.0,(nx)*dx,(nz)*dz,0.0])
     ax2.set_xlabel('X (km)',fontsize=fsize)
     ax2.set_ylabel('Z (km)',fontsize=fsize)
     ax2.tick_params(labelsize=fsize)
-    plt.savefig('./fig/velflts/imgs/img'+create_inttag(num+iex,10000)+'.png',bbox_inches='tight',dpi=150)
+    plt.savefig('./fig/velflts/imgsnew/img'+create_inttag(num+iex,10000)+'.png',bbox_inches='tight',dpi=150,transparent=True)
     plt.close()
     # Plot the label on the image
     plotseglabel(img[:,:,iex],lbl[:,:,iex],color='red',
-          xlabel='X (km)',ylabel='Z (km)',xmin=0.0,xmax=(nx-1)*dx,
-          zmin=0.0,zmax=(nz-1)*dz,vmin=-3,vmax=3,interp='sinc',fsize1=14,fsize2=7,show=False)
-    plt.savefig('./fig/velflts/lbls/lbl'+create_inttag(num+iex,10000)+'.png',bbox_inches='tight',dpi=150)
+          xlabel='X (km)',ylabel='Z (km)',xmin=0.0,xmax=(nx)*dx,labelsize=fsize,ticksize=fsize,
+          zmin=0.0,zmax=(nz)*dz,vmin=-3,vmax=3,interp='sinc',wbox=8,hbox=6,show=False)
+    plt.savefig('./fig/velflts/lblsnew/lbl'+create_inttag(num+iex,10000)+'.png',bbox_inches='tight',dpi=150,transparent=True)
+    plt.close()
+    # Plot the reflectivity
+    fig4 = plt.figure(4,figsize=(8,6)); ax4 = fig4.gca()
+    im4 = ax4.imshow(ref[:,:,iex],cmap='gray',interpolation='bilinear',vmin=-100,vmax=100,
+        extent=[0.0,(nx)*dx,(nz)*dz,0.0])
+    ax4.set_xlabel('X (km)',fontsize=fsize)
+    ax4.set_ylabel('Z (km)',fontsize=fsize)
+    ax4.tick_params(labelsize=fsize)
+    plt.savefig('./fig/velflts/refsnew/ref'+create_inttag(num+iex,10000)+'.png',bbox_inches='tight',dpi=150,transparent=True)
     plt.close()
 
