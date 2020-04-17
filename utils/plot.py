423305ee3d9eff6f41ea127b8e26ee29b4ca4468
ke0m <joseph.s.jennings@gmail.com>
2020-04-17 15:59:15 -0700

    Merging Perlin noise example

--- utils/plot.py
+++ utils/plot.py
@@ -224,3 +224,106 @@ def plot_anggatrhos(aimg,xloc,dz,dx,oro,dro,transp=False,figname=None,ftype='png
     plt.savefig(figname+'.'+ftype,bbox_inches='tight',dpi=150,transparent=True)
     plt.close()
 
+def plot3d(data,os=[0.0,0.0,0.0],ds=[1.0,1.0,1.0],show=True,**kwargs):
+  """
+  Makes a 3D plot of a data cube
+
+  Parameters:
+    data - input 3D data cube
+    os   - origins of each axis [0.0,0.0,0.0]
+    ds   - samplings of each axis [1.0,1.0,1.0]
+  """
+  # Transpose if requested
+  if(not kwargs.get('transp',False)):
+    data = np.expand_dims(data,axis=0)
+    data = np.transpose(data,(0,1,3,2))
+  else:
+    data = (np.expand_dims(data,axis=-1)).T
+    data = np.transpose(data,(0,1,3,2))
+  # Get the shape of the cube
+  ns = np.flip(data.shape)
+  # Make the coordinates for the cross hairs
+  ds = np.append(np.flip(ds),1.0)
+  os = np.append(np.flip(os),0.0)
+  x1=np.linspace(os[0], os[0] + ds[0]*(ns[0]), ns[0])
+  x2=np.linspace(os[1], os[1] + ds[1]*(ns[1]), ns[1])
+  x3=np.linspace(os[2], os[2] + ds[2]*(ns[2]), ns[2])
+
+  # Compute plotting min and max
+  if(kwargs.get('vmin',None) == None or kwargs.get('vmax',None) == None):
+    vmin = np.min(data)*kwargs.get('pclip',0.9)
+    vmax = np.max(data)*kwargs.get('pclip',0.9)
+
+  loc1 = kwargs.get('loc1',int(ns[0]/2*ds[0]+os[0]))
+  i1 = int((loc1 - os[0])/ds[0])
+  loc2 = kwargs.get('loc2',int(ns[1]/2*ds[1]+os[1]))
+  i2 = int((loc2 - os[1])/ds[1])
+  loc3 = kwargs.get('loc3',int(ns[2]/2*ds[2]+os[2]))
+  i3 = int((loc3 - os[2])/ds[2])
+  ax1 = None; ax2 = None; ax3 = None; ax4 = None
+  curr_pos = 0 
+
+  # Axis labels
+  label1 = kwargs.get('label1',' '); label2 = kwargs.get('label2',' '); label3 = kwargs.get('label3', ' ')
+
+  width1 = kwargs.get('width1',4.0); width2 = kwargs.get('width2',4.0); width3 = kwargs.get('width3',4.0)
+  widths=[width1,width3]
+  heights=[width3,width2]
+  gs_kw=dict(width_ratios=widths,height_ratios=heights)
+  fig,ax=plt.subplots(2,2,figsize=(width1+width3,width2+width3),gridspec_kw=gs_kw)
+  plt.subplots_adjust(wspace=0,hspace=0)
+
+  title = kwargs.get('title',' ')
+  ax[0,1].text(0.5,0.5,title[curr_pos],horizontalalignment='center',verticalalignment='center',fontsize=50)
+
+  ## xz plane
+  ax[1,0].imshow(data[curr_pos,:,i2,:],interpolation=kwargs.get('interp','none'),aspect='auto',
+      extent=[os[0],os[0]+(ns[0])*ds[0],os[2]+ds[2]*(ns[2]),os[2]],vmin=vmin,vmax=vmax,cmap=kwargs.get('cmap','gray'))
+  ax[1,0].tick_params(labelsize=kwargs.get('ticksize',14))
+  ax[1,0].plot(loc1*np.ones((ns[2],)),x3,c='k')
+  ax[1,0].plot(x1,loc3*np.ones((ns[0],)),c='k')
+  ax[1,0].set_xlabel(label1,fontsize=kwargs.get('labelsize',14))
+  ax[1,0].set_ylabel(label3,fontsize=kwargs.get('labelsize',14))
+
+  # yz plane
+  im = ax[1,1].imshow(data[curr_pos,:,:,i1],interpolation=kwargs.get('interp','none'),aspect='auto',
+      extent=[os[1],os[1]+(ns[1])*ds[1],os[2]+(ns[2])*ds[2],os[2]],vmin=vmin,vmax=vmax,cmap=kwargs.get('cmap','gray'))
+  ax[1,1].tick_params(labelsize=kwargs.get('ticksize',14))
+  ax[1,1].plot(loc2*np.ones((ns[2],)),x3,c='k')
+  ax[1,1].plot(x2,loc3*np.ones((ns[1],)),c='k')
+  ax[1,1].get_yaxis().set_visible(False)
+  ax[1,1].set_xlabel(label2,fontsize=kwargs.get('labelsize',14))
+  ax1=ax[1,1].twinx()
+  ax1.set_ylim(ax[1,1].get_ylim())
+  ax1.set_yticks([loc3])
+  ax1.set_yticklabels(['%.2f'%(loc3)],rotation='vertical',va='center')
+  ax1.tick_params(labelsize=kwargs.get('ticksize',14))
+  ax2=ax[1,1].twiny()
+  ax2.set_xlim(ax[1,1].get_xlim())
+  ax2.set_xticks([loc2])
+  ax2.set_xticklabels(['%.2f'%(loc2)])
+  ax2.tick_params(labelsize=kwargs.get('ticksize',14))
+
+  # xy plane
+  ax[0,0].imshow(np.flip(data[curr_pos,i3,:,:],0),interpolation=kwargs.get('interp','none'),aspect='auto',
+      extent=[os[0],os[0]+(ns[0])*ds[0],os[1],os[1]+(ns[1])*ds[1]],vmin=vmin,vmax=vmax,cmap=kwargs.get('cmap','gray'))
+  ax[0,0].tick_params(labelsize=kwargs.get('ticksize',14))
+  ax[0,0].plot(loc1*np.ones((ns[1],)),x2,c='k')
+  ax[0,0].plot(x1,loc2*np.ones((ns[0],)),c='k')
+  ax[0,0].set_ylabel(label2,fontsize=kwargs.get('labelsize',14))
+  ax[0,0].get_xaxis().set_visible(False)
+  ax3=ax[0,0].twinx()
+  ax3.set_ylim(ax[0,0].get_ylim())
+  ax3.set_yticks([loc2])
+  ax3.set_yticklabels(['%.2f'%(loc2)],rotation='vertical',va='center')
+  ax3.tick_params(labelsize=kwargs.get('ticksize',14))
+  ax4=ax[0,0].twiny()
+  ax4.set_xlim(ax[0,0].get_xlim())
+  ax4.set_xticks([loc1])
+  ax4.set_xticklabels(['%.2f'%(loc1)])
+  ax4.tick_params(labelsize=kwargs.get('ticksize',14))
+  
+  ax[0,1].axis('off')
+  if(show):
+    plt.show()
+
