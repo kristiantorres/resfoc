2b56702515d135917459000e0fb9a9c483dfd21f
ke0m <joseph.s.jennings@gmail.com>
2020-04-19 23:45:09 -0700

    Merging changes from bench-rojas

--- resfoc/resmig.py
+++ resfoc/resmig.py
@@ -55,7 +55,7 @@ def preresmig(img,ds,nro=6,oro=1.0,dro=0.01,nps=None,time=True,transp=False,debu
   if(verb): print("Padding to size nhp=%d nmp=%d nzp=%d"%(imgp.shape[0],imgp.shape[1],imgp.shape[2]))
   imgpft = cft.cosft(imgp,axis0=1,axis1=1,axis2=1,verb=True)
   # Compute samplings
-  dcs = cft.samplings(imgpft,ds)
+  dcs = cft.samplings(imgpft,ids)
 
   # Residual migration
   nzpc = imgpft.shape[2]; nmpc = imgpft.shape[1]; nhpc = imgpft.shape[0]
