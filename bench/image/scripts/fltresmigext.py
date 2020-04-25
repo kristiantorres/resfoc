ea936ffecbd7381d21bd1c02f03b343d1e8b3e02
ke0m <joseph.s.jennings@gmail.com>
2020-04-25 14:58:02 -0700

    3D plotting utility

--- bench/image/scripts/fltresmigext.py
+++ bench/image/scripts/fltresmigext.py
@@ -1,36 +1,31 @@
 import inpout.seppy as seppy
 import numpy as np
-from resfoc.resmig import preresmig, convert2time
+from resfoc.resmig import preresmig, convert2time, get_rho_axis
 import matplotlib.pyplot as plt
 from utils.movie import viewimgframeskey
-from utils.signal import butter_bandpass_filter
 
 # Read in the processed fault image
-sep = seppy.sep([])
-iaxes,img = sep.read_file(None,ifname='fltimgextprc.H')
+sep = seppy.sep()
+iaxes,img = sep.read_file('fltimgbig.H')
 img = img.reshape(iaxes.n,order='F')
 
-imgt = np.ascontiguousarray(np.transpose(img,(2,1,0)))
+imgt = np.ascontiguousarray(img.T).astype('float32')
 
 # Get axes
-nz  = iaxes.n[0]; oz = iaxes.o[0]; dz = iaxes.d[0]
-nx  = iaxes.n[1]; ox = iaxes.o[1]; dx = iaxes.d[1]
-rnh = iaxes.n[2]; oh = iaxes.o[2]; dh = iaxes.d[2]
-
-# Make axes
-nh  = 10 ; oh  = -200;  dh  = dx
-nro = 6  ; oro = 1.0 ;  dro = 0.01
-zoff = 10
+[nz,nx,nh] = iaxes.n; [dz,dx,dh] = iaxes.d; [oz,ox,oh] = iaxes.o
 
 # Depth Residual migration
-rmig = preresmig(imgt,[dh,dx,dz],time=False,nthreads=2*nro-1)
-
-print(rmig.shape)
+inro = 17; idro = 0.00125
+onro,ooro,odro = get_rho_axis(nro=inro,dro=idro)
+rmig = preresmig(imgt,[dh,dx,dz],nps=[2049,1025,513],nro=inro,dro=idro,time=False,nthreads=24,verb=True)
 
 # Conversion to time
-time = convert2time(rmig,dz,dt=0.004)
+time = convert2time(rmig,dz,dt=0.004,dro=odro,verb=True)
 
 # Visualize the frames
-viewimgframeskey(rmig[:,zoff,:,:],ttlstring='rho=%.2f',ottl=oro-dro*(nro-1),dttl=dro,wbox=14,hbox=7,pclip=0.9,show=False)
-viewimgframeskey(time[:,zoff,:,:],ttlstring='rho=%.2f',ottl=oro-dro*(nro-1),dttl=dro,wbox=14,hbox=7,pclip=0.9,show=True)
+#viewimgframeskey(rmig[:,16,:,:],ttlstring='rho=%.2f',ottl=ooro,dttl=odro,wbox=14,hbox=7,pclip=0.9,show=False)
+#viewimgframeskey(time[:,16,:,:],ttlstring='rho=%.2f',ottl=ooro,dttl=odro,wbox=14,hbox=7,pclip=0.9,show=True)
+
+# Write the subsurface offsets to file
+sep.write_file("fltimgbigres.H",rmig.T,ds=[dz,dx,dh,odro],os=[0,0,oh,ooro])
 
