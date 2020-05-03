22a305d18ab45aff94b05f296dbe4cabca46a453
ke0m <joseph.s.jennings@gmail.com>
2020-05-02 20:19:52 -0700

    Merging from master

--- utils/plot.py
+++ utils/plot.py
@@ -2,7 +2,7 @@
 Useful functions for plotting. No interactive plots.
 See utils.movie for interactive plotting
 @author: Joseph Jennings
-@version: 2020.03.24
+@version: 2020.04.28
 """
 import numpy as np
 from utils.signal import ampspec1d
@@ -131,7 +131,7 @@ def plot_allanggats(aimg,dz,dx,jx=10,transp=False,show=True,**kwargs):
     aimgt = np.transpose(aimg,(2,0,1))
   else:
     aimgt = aimg
-  nz = aimgt.shape[2]; na = aimgt.shape[1]; nx = aimg.shape[2]
+  nz = aimgt.shape[2]; na = aimgt.shape[1]; nx = aimgt.shape[0]
   # Subsample the spatial axis
   aimgts = aimgt[::jx,:,:]
   nxs = aimgts.shape[0]
@@ -188,7 +188,7 @@ def plot_anggatrhos(aimg,xloc,dz,dx,oro,dro,transp=False,figname=None,ftype='png
   lz = np.linspace(izmin*dz,izmax*dz,izmax-izmin)
   lx = np.zeros(izmax-izmin) + xloc*dx
   vmin1 = np.min(mig); vmax1 = np.max(mig)
-  ax1.imshow(mig[kwargs.get('xmin'):kwargs.get('xmax'),kwargs.get('zmin',0):kwargs.get('zmax',nz)].T,cmap='gray',
+  ax1.imshow(mig[kwargs.get('xmin',0):kwargs.get('xmax',nx),kwargs.get('zmin',0):kwargs.get('zmax',nz)].T,cmap='gray',
       interpolation=kwargs.get('interp','sinc'),extent=[kwargs.get('xmin',0)*dx,
     kwargs.get('xmax',nx)*dx,izmax*dz,izmin*dz],vmin=vmin1*kwargs.get('pclip',1.0),vmax=vmax1*kwargs.get('pclip',1.0))
   ax1.plot(lx,lz,color='k',linewidth=2)
@@ -223,4 +223,242 @@ def plot_anggatrhos(aimg,xloc,dz,dx,oro,dro,transp=False,figname=None,ftype='png
   if(figname is not None):
     plt.savefig(figname+'.'+ftype,bbox_inches='tight',dpi=150,transparent=True)
     plt.close()
+
+def plot_imgvelptb(img,velptb,dz,dx,thresh,agc=True,alpha=0.3,show=False,figname=None,**kwargs):
+  """
+  Plots a velocity perturbation on top of an image
+
+  Parameters
+    focimg - the input image
+    velptb - the velocity perturbation
+    dz     - depth sampling interval
+    dx     - horizontal sampling interval
+    thresh - threshold in velocity to apply
+    agc    - apply AGC to the image before plotting [True]
+    alpha  - transparence value [0.3]
+    xmin   - the minimum x sample to plot for windowing [0]
+    xmax   - the maximum x sample to plot for windowing [nx]
+    zmin   - the minimum z sample to plot for windowing [0]
+    zmax   - the maximum z sample to plot for windowing [nz]
+    pclip  - pclip to apply for gain                    [1.0]
+  """
+  if(img.shape != velptb.shape):
+    raise Exception("Image and velocity must have same shape")
+  # Get spatial plotting range
+  [nz,nx] = img.shape;
+  ixmin = kwargs.get('xmin',0); ixmax = kwargs.get('xmax',nx)
+  xmin = ixmin*dx; xmax = ixmax*dx
+  izmin = kwargs.get('zmin',0); izmax = kwargs.get('zmax',nz)
+  zmin = izmin*dz; zmax = izmax*dz
+  # Get amplitude range
+  ivmin = np.min(img);    ivmax = np.max(img)
+  pvmin = kwargs.get('velmin',np.min(velptb)); pvmax = kwargs.get('velmax',np.max(velptb))
+  pclip = kwargs.get('pclip',1.0)
+  # Plot the perturbation to get the true colorbar
+  fig1 = plt.figure(figsize=(kwargs.get('wbox',10),kwargs.get('hbox',6)))
+  ax1 = fig1.gca()
+  im1 = ax1.imshow(velptb[izmin:izmax,ixmin:ixmax],cmap='seismic',
+      extent=[ixmin*dx/1000,(ixmax-1)*dx/1000.0,izmax*dz/1000.0,izmin*dz/1000.0],interpolation='bilinear',
+      vmin=pvmin,vmax=pvmax)
+  plt.close()
+  # Plot perturbation on the image
+  fig = plt.figure(figsize=(kwargs.get('wbox',10),kwargs.get('hbox',6)))
+  ax = fig.gca()
+  if(agc):
+    gimg = agc(img.astype('float32').T).T
+  else:
+    gimg = img
+  ax.imshow(gimg[izmin:izmax,ixmin:ixmax],vmin=ivmin*pclip,vmax=ivmax*pclip,
+             extent=[ixmin*dx/1000,(ixmax)*dx/1000.0,izmax*dz/1000.0,izmin*dz/1000.0],cmap='gray',interpolation='sinc')
+  mask1 = np.ma.masked_where((velptb) < thresh, velptb)
+  mask2 = np.ma.masked_where((velptb) > -thresh, velptb)
+  ax.imshow(mask1[izmin:izmax,ixmin:ixmax],extent=[ixmin*dx/1000,ixmax*dx/1000.0,izmax*dz/1000.0,izmin*dz/1000.0],alpha=alpha,
+      cmap='seismic',vmin=pvmin,vmax=pvmax,interpolation='bilinear')
+  ax.imshow(mask2[izmin:izmax,ixmin:ixmax],extent=[ixmin*dx/1000,ixmax*dx/1000.0,izmax*dz/1000.0,izmin*dz/1000.0],alpha=alpha,
+      cmap='seismic',vmin=pvmin,vmax=pvmax,interpolation='bilinear')
+  ax.set_xlabel('X (km)',fontsize=kwargs.get('labelsize',15))
+  ax.set_ylabel('Z (km)',fontsize=kwargs.get('labelsize',15))
+  ax.tick_params(labelsize=kwargs.get('labelsize',15))
+  # Colorbar
+  cbar_ax = fig.add_axes([kwargs.get('barx',0.91),kwargs.get('barz',0.15),kwargs.get('wbar',0.02),kwargs.get('hbar',0.70)])
+  cbar = fig.colorbar(im1,cbar_ax,format='%.0f')
+  cbar.ax.tick_params(labelsize=kwargs.get('labelsize',15))
+  cbar.set_label('Velocity (m/s)',fontsize=kwargs.get('labelsize',15))
+  if(figname is not None):
+    plt.savefig(figname,bbox_inches='tight',transparent=True,dpi=150)
+  if(show):
+    plt.show()
+  plt.close()
+
+def plot3d(data,os=[0.0,0.0,0.0],ds=[1.0,1.0,1.0],show=True,**kwargs):
+  """
+  Makes a 3D plot of a data cube
+
+  Parameters:
+    data - input 3D data cube
+    os   - origins of each axis [0.0,0.0,0.0]
+    ds   - samplings of each axis [1.0,1.0,1.0]
+  """
+  # Transpose if requested
+  if(not kwargs.get('transp',False)):
+    data = np.expand_dims(data,axis=0)
+    data = np.transpose(data,(0,1,3,2))
+  else:
+    data = (np.expand_dims(data,axis=-1)).T
+    data = np.transpose(data,(0,1,3,2))
+  # Get the shape of the cube
+  ns = np.flip(data.shape)
+  # Make the coordinates for the cross hairs
+  ds = np.append(np.flip(ds),1.0)
+  os = np.append(np.flip(os),0.0)
+  x1=np.linspace(os[0], os[0] + ds[0]*(ns[0]), ns[0])
+  x2=np.linspace(os[1], os[1] + ds[1]*(ns[1]), ns[1])
+  x3=np.linspace(os[2], os[2] + ds[2]*(ns[2]), ns[2])
+
+  # Compute plotting min and max
+  if(kwargs.get('vmin',None) == None or kwargs.get('vmax',None) == None):
+    vmin = np.min(data)*kwargs.get('pclip',1.0)
+    vmax = np.max(data)*kwargs.get('pclip',1.0)
+
+  loc1 = kwargs.get('loc1',int(ns[0]/2*ds[0]+os[0]))
+  i1 = int((loc1 - os[0])/ds[0])
+  loc2 = kwargs.get('loc2',int(ns[1]/2*ds[1]+os[1]))
+  i2 = int((loc2 - os[1])/ds[1])
+  loc3 = kwargs.get('loc3',int(ns[2]/2*ds[2]+os[2]))
+  i3 = int((loc3 - os[2])/ds[2])
+  ax1 = None; ax2 = None; ax3 = None; ax4 = None
+  curr_pos = 0
+
+  # Axis labels
+  label1 = kwargs.get('label1',' '); label2 = kwargs.get('label2',' '); label3 = kwargs.get('label3', ' ')
+
+  width1 = kwargs.get('width1',4.0); width2 = kwargs.get('width2',4.0); width3 = kwargs.get('width3',4.0)
+  widths=[width1,width3]
+  heights=[width3,width2]
+  gs_kw=dict(width_ratios=widths,height_ratios=heights)
+  fig,ax=plt.subplots(2,2,figsize=(width1+width3,width2+width3),gridspec_kw=gs_kw)
+  plt.subplots_adjust(wspace=0,hspace=0)
+
+  title = kwargs.get('title',' ')
+  ax[0,1].text(0.5,0.5,title[curr_pos],horizontalalignment='center',verticalalignment='center',fontsize=50)
+
+  ## xz plane
+  ax[1,0].imshow(data[curr_pos,:,i2,:],interpolation=kwargs.get('interp','none'),aspect='auto',
+      extent=[os[0],os[0]+(ns[0])*ds[0],os[2]+ds[2]*(ns[2]),os[2]],vmin=vmin,vmax=vmax,cmap=kwargs.get('cmap','gray'))
+  ax[1,0].tick_params(labelsize=kwargs.get('ticksize',14))
+  ax[1,0].plot(loc1*np.ones((ns[2],)),x3,c='k')
+  ax[1,0].plot(x1,loc3*np.ones((ns[0],)),c='k')
+  ax[1,0].set_xlabel(label1,fontsize=kwargs.get('labelsize',14))
+  ax[1,0].set_ylabel(label3,fontsize=kwargs.get('labelsize',14))
+
+  # yz plane
+  im = ax[1,1].imshow(data[curr_pos,:,:,i1],interpolation=kwargs.get('interp','none'),aspect='auto',
+      extent=[os[1],os[1]+(ns[1])*ds[1],os[2]+(ns[2])*ds[2],os[2]],vmin=vmin,vmax=vmax,cmap=kwargs.get('cmap','gray'))
+  ax[1,1].tick_params(labelsize=kwargs.get('ticksize',14))
+  ax[1,1].plot(loc2*np.ones((ns[2],)),x3,c='k')
+  ax[1,1].plot(x2,loc3*np.ones((ns[1],)),c='k')
+  ax[1,1].get_yaxis().set_visible(False)
+  ax[1,1].set_xlabel(label2,fontsize=kwargs.get('labelsize',14))
+  ax1=ax[1,1].twinx()
+  ax1.set_ylim(ax[1,1].get_ylim())
+  ax1.set_yticks([loc3])
+  ax1.set_yticklabels(['%.2f'%(loc3)],rotation='vertical',va='center')
+  ax1.tick_params(labelsize=kwargs.get('ticksize',14))
+  ax2=ax[1,1].twiny()
+  ax2.set_xlim(ax[1,1].get_xlim())
+  ax2.set_xticks([loc2])
+  ax2.set_xticklabels(['%.2f'%(loc2)])
+  ax2.tick_params(labelsize=kwargs.get('ticksize',14))
+
+  # xy plane
+  ax[0,0].imshow(np.flip(data[curr_pos,i3,:,:],0),interpolation=kwargs.get('interp','none'),aspect='auto',
+      extent=[os[0],os[0]+(ns[0])*ds[0],os[1],os[1]+(ns[1])*ds[1]],vmin=vmin,vmax=vmax,cmap=kwargs.get('cmap','gray'))
+  ax[0,0].tick_params(labelsize=kwargs.get('ticksize',14))
+  ax[0,0].plot(loc1*np.ones((ns[1],)),x2,c='k')
+  ax[0,0].plot(x1,loc2*np.ones((ns[0],)),c='k')
+  ax[0,0].set_ylabel(label2,fontsize=kwargs.get('labelsize',14))
+  ax[0,0].get_xaxis().set_visible(False)
+  ax3=ax[0,0].twinx()
+  ax3.set_ylim(ax[0,0].get_ylim())
+  ax3.set_yticks([loc2])
+  ax3.set_yticklabels(['%.2f'%(loc2)],rotation='vertical',va='center')
+  ax3.tick_params(labelsize=kwargs.get('ticksize',14))
+  ax4=ax[0,0].twiny()
+  ax4.set_xlim(ax[0,0].get_xlim())
+  ax4.set_xticks([loc1])
+  ax4.set_xticklabels(['%.2f'%(loc1)])
+  ax4.tick_params(labelsize=kwargs.get('ticksize',14))
+
+  ax[0,1].axis('off')
+  if(show):
+    plt.show()
+
+def plot_rhopicks(ang,smb,pck,dro,dz,oro,oz=0.0,agc=False,mode='sbs',show=True,figname=None,ftype='png',**kwargs):
+  """
+  Plots the semblance picks on top of the computed semblance panel
+  and the residually migrated angle gathers
+
+  Parameters:
+    ang   - Residually migrated angle gathers [nro,na,nz]
+    smb   - Computed rho semblance [nro,nz]
+    pck   - The computed Rho picks [nz]
+    dz    - The depth sampling
+    dro   - The residual migration sampling
+    oro   - The residual migration origin
+    agc   - Apply agc to the gathers [False]
+    mode  - Mode of how to plot ([sbs]/tb) side by side or top/bottom
+    show  - Show the plots [True]
+    fname - Output figure name [None]
+  """
+  # Reshape the angle gathers
+  nro = ang.shape[0]; na = ang.shape[1]; nz = ang.shape[2]
+  angr = ang.reshape([na*nro,nz])
+  # Gain the data
+  if(agc):
+    angrg = agc(angr)
+  else:
+    angr  = angr
+  vmin = np.min(angr); vmax = np.max(angr)
+  pclip = kwargs.get('pclip',1.0)
+  # Compute z for rho picks
+  z = np.linspace(oz,oz+(nz-1)*dz,nz)
+  # Plot the rho picks
+  wbox = kwargs.get('wbox',14); hbox = kwargs.get('hbox',7)
+  fntsize = kwargs.get('fontsize',15); tcksize = kwargs.get('ticksize',15)
+  if(mode == 'sbs'):
+    fig,ax = plt.subplots(1,2,figsize=(wbox,hbox))
+    # Angle gather
+    ax[0].imshow(angr.T,cmap='gray',aspect=0.009,extent=[oro,oro+(nro)*dro,nz*dz,0.0],interpolation='sinc',
+               vmin=vmin*pclip,vmax=vmax*pclip)
+    ax[0].plot(pck,z,linewidth=3,color='tab:cyan')
+    ax[0].set_xlabel(r'$\rho$',fontsize=fntsize)
+    ax[0].set_ylabel('Z (km)',fontsize=fntsize)
+    ax[0].tick_params(labelsize=tcksize)
+    # Semblance
+    ax[1].imshow(smb.T,cmap='jet',aspect=0.02,extent=[oro,oro+(nro)*dro,nz*dz,0.0],interpolation='bilinear')
+    ax[1].plot(pck,z,linewidth=3,color='k')
+    ax[1].set_xlabel(r'$\rho$',fontsize=fntsize)
+    ax[1].set_ylabel(' ',fontsize=fntsize)
+    ax[1].tick_params(labelsize=tcksize)
+    plt.subplots_adjust(wspace=-0.4)
+  elif(mode == 'tb'):
+    fig,ax = plt.subplots(2,1,figsize=(wbox,hbox))
+    # Angle gather
+    ax[0].imshow(angr.T,cmap='gray',aspect=0.009,extent=[oro,oro+(nro)*dro,nz*dz,0.0],interpolation='sinc',
+               vmin=vmin*pclip,vmax=vmax*pclip)
+    ax[0].plot(pck,z,linewidth=3,color='tab:cyan')
+    ax[0].set_xlabel(r'$\rho$',fontsize=fntsize)
+    ax[0].set_ylabel('Z (km)',fontsize=fntsize)
+    ax[0].tick_params(labelsize=tcksize)
+    # Semblance
+    ax[1].imshow(smb.T,cmap='jet',aspect=0.009,extent=[oro,oro+(nro)*dro,nz*dz,0.0],interpolation='bilinear')
+    ax[1].plot(pck,z,linewidth=3,color='k')
+    ax[1].set_xlabel(r'$\rho$',fontsize=fntsize)
+    ax[1].set_ylabel(' ',fontsize=fntsize)
+    ax[1].tick_params(labelsize=tcksize)
+  if(figname is not None):
+    plt.savefig(figname+'.'+ftype,bbox_inches='tight',dpi=150,transparent=True)
+    plt.close()
+  if(show):
+    plt.show()
 
