266fbeaf5924445fc14831a9d95de6794bf325ea
ke0m <joseph.s.jennings@gmail.com>
2020-05-12 08:12:03 -0700

    Focus and defocus migration functions

---
+++ bench/scripts/randund_fault_focus.py
@@ -0,0 +1,131 @@
+"""
+Creates a well-focused fault image. Generated image
+contains randomly undulating layers and faults
+
+@author: Joseph Jennings
+@version: 2020.05.11
+"""
+import sys, os, argparse, configparser
+import numpy as np
+import inpout.seppy as seppy
+from velocity.stdmodels import undulatingrandfaults2d
+from scaas.velocity import create_randomptbs_loc
+from scaas.trismooth import smooth
+import scaas.defaultgeom as geom
+from scaas.wavelet import ricker
+from utils.signal import ampspec2d
+from utils.movie import viewimgframeskey
+
+# Parse the config file
+conf_parser = argparse.ArgumentParser(add_help=False)
+conf_parser.add_argument("-c", "--conf_file",
+                         help="Specify config file", metavar="FILE")
+args, remaining_argv = conf_parser.parse_known_args()
+defaults = {
+    "fx": 373,
+    "nxw": 512,
+    "na": 64,
+    }
+if args.conf_file:
+  config = configparser.ConfigParser()
+  config.read([args.conf_file])
+  defaults = dict(config.items("defaults"))
+
+# Parse the other arguments
+# Don't surpress add_help here so it will handle -h
+parser = argparse.ArgumentParser(parents=[conf_parser],description=__doc__,
+    formatter_class=argparse.RawDescriptionHelpFormatter)
+
+# Set defaults
+parser.set_defaults(**defaults)
+
+# Input files
+ioArgs = parser.add_argument_group('Inputs and outputs')
+ioArgs.add_argument("-vel",help="Output velocity model",type=str,required=True)
+ioArgs.add_argument("-ptb",help="Output velocity perturbation",type=str,required=True)
+ioArgs.add_argument("-ref",help="Output reflectivity",type=str,required=True)
+ioArgs.add_argument("-lbl",help="Output label",type=str,required=True)
+ioArgs.add_argument("-fimgo",help="Focused migrated image (offset)",type=str,required=True)
+ioArgs.add_argument("-fimga",help="Focused migrated image (angle)",type=str,required=True)
+ioArgs.add_argument("-dimgo",help="Defocused migrated image (offset)",type=str,required=True)
+ioArgs.add_argument("-dimga",help="Defocused migrated image (angle)",type=str,required=True)
+# Optional arguments
+parser.add_argument("-fx",help="First x sample for window [373]",type=int)
+parser.add_argument("-nxw",help="Size of window [512]",type=int)
+parser.add_argument("-na",help="Number of angles to compute [64]",type=int)
+# Enables required arguments in config file
+for action in parser._actions:
+  if(action.dest in defaults):
+    action.required = False
+args = parser.parse_args(remaining_argv)
+
+# Create IO for writing at the end
+sep = seppy.sep()
+
+# Create layered model
+nx = 1300; nz = 512
+vel,ref,cnv,lbl = undulatingrandfaults2d(nz=nz,nx=nx,ofx=0.4,dfx=0.08)
+dx = 10; dz = 10
+
+# Create migration velocity
+velmig = smooth(vel,rect1=30,rect2=30)
+
+# Create a random perturbation
+ano = create_randomptbs_loc(nz,nx,nptbs=3,romin=0.95,romax=1.05,
+                            minnaz=100,maxnaz=150,minnax=100,maxnax=400,mincz=100,maxcz=150,mincx=300,maxcx=900,
+                            mindist=100,nptsz=2,nptsx=2,octaves=2,period=80,persist=0.2,ncpu=1,sigma=20)
+
+# Create velocity with anomaly
+veltru = velmig*ano
+velptb = veltru - velmig
+
+# Acquisition geometry
+dsx = 20; bx = 50; bz = 50
+prp = geom.defaultgeom(nx,dx,nz,dz,nsx=66,dsx=dsx,bx=bx,bz=bz)
+
+# Create data axes
+ntu = 6500; dtu = 0.001;
+freq = 20; amp = 100.0; dly = 0.2;
+wav = ricker(ntu,dtu,freq,amp,dly)
+
+# Model true linearized data
+dtd = 0.008
+allshot = prp.model_lindata(veltru,ref,wav,dtd,verb=True,nthrds=24)
+
+# Taper for migration
+prp.build_taper(70,150)
+
+# Wave equation depth migration (both with anomaly)
+imgr = prp.wem(veltru,allshot,wav,dtd,nh=16,lap=True,verb=True,nthrds=20)
+imgw = prp.wem(velmig,allshot,wav,dtd,nh=16,lap=True,verb=True,nthrds=20)
+nh,oh,dh = prp.get_off_axis()
+
+# Window to the region of interest
+nxw = args.nxw; nw = args.fx
+imgrw = imgr[:,:,nw:nw+nxw]
+imgww = imgw[:,:,nw:nw+nxw]
+lblw  = lbl[:,nw:nw+nxw]
+
+# Convert to angle gathers
+imgrang = prp.to_angle(imgrw,na=args.na,verb=True,nthrds=24)
+imgwang = prp.to_angle(imgww,na=args.na,verb=True,nthrds=24)
+na,oa,da = prp.get_ang_axis()
+
+# Window and transpose the image and the label
+imgrwt = np.transpose(imgrw,(0,2,1)) # [nh,nz,nx] -> [nh,nx,nz]
+imgwwt = np.transpose(imgww,(0,2,1)) # [nh,nz,nx] -> [nh,nx,nz]
+
+# Output inputs
+sep.write_file(args.vel,vel,ds=[dz,dx],dpath=args.dpath+'/')
+sep.write_file(args.ref,ref,ds=[dz,dx],dpath=args.dpath+'/')
+sep.write_file(args.lbl,lblw,ds=[dz,dx],dpath=args.dpath+'/')
+sep.write_file(args.ptb,-velptb,ds=[dz,dx],dpath=args.dpath+'/')
+# Output images
+sep.write_file(args.fimgo,imgrwt.T,ds=[dz,dx,dh],os=[0,0,oh],dpath=args.dpath+'/')
+sep.write_file(args.fimga,imgrang.T,ds=[dz,da,dx],os=[0,oa,0],dpath=args.dpath+'/')
+sep.write_file(args.dimgo,imgwwt.T,ds=[dz,dx,dh],os=[0,0,oh],dpath=args.dpath+'/')
+sep.write_file(args.dimga,imgwang.T,ds=[dz,da,dx],os=[0,oa,0],dpath=args.dpath+'/')
+
+# Flag for cluster manager
+print("Success!")
+
---
+++ cluster/slurmmanager.py
@@ -0,0 +1,168 @@
+"""
+A job manager for a slurm cluster
+
+@author: Joseph Jennings
+@version: 2020.05.11
+"""
+import cluster.slurmhelper as slurm
+import subprocess, glob
+import time
+
+def manage_slurmjobs(actjobs,bcmd,jobprefix,lefjobs,ajobs,nprocs=48,nleft=2,nsubmit=5,
+                     sepqfull=False,hr2qfull=False,delay=5.0,verb=True):
+  """
+  A job manager for a slurm cluster
+
+  Parameters
+    actjobs  - a list of job objects that have been submitted to the
+              cluster
+    lefjobs  - a list of job objects that have yet to be submitted
+    ajobs    - number of active jobs to maitain running
+    logpath  - path to logfiles
+    parpath  - path to par files
+    sepqfull - flag indicating whether the sep queue is full
+    hr2qfull - flag indicating whether the two hour queue is full
+    delay    - delay between checking all jobs [5.0 seconds]
+    verb     - verbosity flag [True]
+  """
+  # Loop until all jobs have completed
+  while len(actjobs) > 0:
+    todel = []
+    # First update qstat and squeue
+    qlines = slurm.qstat(); qqueue = slurm.squeue()
+    sepq = slurm.get_numjobs('sep',qfile=qlines)
+    hr2q = slurm.get_numjobs('twohour',qfile=qlines)
+    # Check the status of each job
+    for ijob in range(len(actjobs)):
+      actjobs[ijob].getstatus_fast(qlines,qqueue)
+      if(verb):
+        print("Job=%d %s sep: %s %s twohour: %s %s"%(ijob, actjobs[ijob].jobid,
+          actjobs[ijob].status['sep'],     actjobs[ijob].nodes['sep'],
+          actjobs[ijob].status['twohour'], actjobs[ijob].nodes['twohour']))
+      if(actjobs[ijob].status['sep'] == None and actjobs[ijob].status['twohour'] == None):
+        # Resubmit if None and None
+        if(sepq['Q'] < nleft):
+          cmd = bcmd + actjobs[ijob].pfname
+          actjobs[ijob].submit(jobprefix,cmd,nprocs=nprocs,queue='sep',sleep=0.5)
+          if(verb): print("Resubmitting stale Job=%d %s to queue sep..."%(ijob,actjobs[ijob].jobid))
+        elif(hr2q['Q'] < nleft):
+          cmd = bcmd + actjobs[ijob].pfname
+          actjobs[ijob].submit(jobprefix,cmd,nprocs=nprocs,queue='twohour',sleep=0.5)
+          if(verb): print("Resubmitting stale Job=%d %s to queue twohour..."%(ijob,actjobs[ijob].jobid))
+        todel.append(False)
+      if('C' in actjobs[ijob].status.values()):
+        # If completed delete or resubmit
+        if(actjobs[ijob].success('Success!')):
+          if(verb): print("Job=%d %s complete!"%(ijob,actjobs[ijob].jobid))
+          todel.append(True)
+        elif(actjobs[ijob].nsub < nsubmit):
+          # First, get the queue of the job
+          myqueue = list(actjobs[ijob].status.keys())[list(actjobs[ijob].status.values()).index('C')]
+          # Check on what node the job failed
+          if(verb):
+            print("Job=%d %s failed %d times on node %s"%(ijob, actjobs[ijob].jobid, actjobs[ijob].nsub, actjobs[ijob].nodes[myqueue]))
+          if(sepq['Q'] < nleft):
+            cmd = bcmd + actjobs[ijob].pfname
+            actjobs[ijob].submit(jobprefix,cmd,nprocs=nprocs,queue='sep',sleep=0.5)
+            if(verb): print("Resubmitting failed Job=%d %s to queue sep..."%(ijob,actjobs[ijob].jobid))
+          elif(hr2q['Q'] < nleft):
+            cmd = bcmd + actjobs[ijob].pfname
+            actjobs[ijob].submit(jobprefix,cmd,nprocs=nprocs,queue='twohour',sleep=0.5)
+            if(verb): print("Resubmitting failed Job=%d %s to queue twohour..."%(ijob,actjobs[ijob].jobid))
+          todel.append(False)
+        else:
+          if(verb): print("Submitted Job=%d %s %d times and failed each time. Removing..."%(ijob,actjobs[ijob].jobid,nsubmit))
+          if(actjobs[ijob].status['sep'] == 'Q'):
+            actjobs[ijob].delete('sep')
+          if(actjobs[ijob].status['twohour'] == 'Q'):
+            actjobs[ijob].delete('twohour')
+          todel.append(True)
+      else:
+        # Leave the job alone
+        todel.append(False)
+    # Delete completed jobs
+    idx = 0
+    for ijob in range(len(actjobs)):
+      if(todel[ijob]):
+        del actjobs[idx]
+      else:
+        idx += 1
+    # Submit and add leftover jobs
+    for ijob in range(len(lefjobs)):
+      # Don't append if no active jobs complete
+      if(len(actjobs) >= ajobs): break
+      cmd = bcmd + lefjobs[0].pfname
+      # Don't submit to a queue that is full
+      if(sepq['Q'] < 2):
+        lefjobs[0].submit(jobprefix,cmd,nprocs=nprocs,queue='sep',sleep=0.5)
+        actjobs.append(lefjobs[0])
+        if(verb): print("Submitting waiting Job %s to sep queue..."%(lefjobs[0].jobid))
+        del lefjobs[0]
+      elif(hr2q['Q'] < 2):
+        lefjobs[0].submit(jobprefix,cmd,nprocs=nprocs,queue='twohour',sleep=0.5)
+        actjobs.append(lefjobs[0])
+        if(verb): print("Submitting waiting Job %s to twohour queue..."%(lefjobs[0].jobid))
+        del lefjobs[0]
+      else:
+        sepqfull = True; hr2qfull = True
+        if(verb): print("Both sep and twohour queues are full. Not submitting for now...")
+    # Make sure that at least two are waiting in both queues
+    sepq = slurm.get_numjobs('sep',qfile=qlines)
+    #TODO: remove the sepqfull and add a while loop while (nleft - sepq['Q'] - 1 > 0) and update sepq each time
+    if(sepqfull and sepq['Q'] < nleft and len(lefjobs) > 0):
+      for ijob in range(nleft - sepq['Q'] - 1):
+        cmd = bcmd + lefjobs[0].pfname
+        lefjobs[0].submit(jobprefix,cmd,nprocs=nprocs,queue='sep',sleep=0.5)
+        actjobs.append(lefjobs[0])
+        if(verb): print("Submitting waiting Job %s in sep for queuing..."%(lefjobs[0].jobid))
+        del lefjobs[0]
+    hr2q = slurm.get_numjobs('twohour',qfile=qlines)
+    if(hr2qfull and hr2q['Q'] < nleft and len(lefjobs) > 0):
+      for ijob in range(nleft - hr2q['Q'] - 1):
+        cmd = bcmd + lefjobs[0].pfname
+        lefjobs[0].submit(jobprefix,cmd,nprocs=nprocs,queue='twohour',sleep=0.5)
+        actjobs.append(lefjobs[0])
+        if(verb): print("Submitting waiting Job %s in twohour for queuing..."%(lefjobs[0].jobid))
+        del lefjobs[0]
+    if(verb):
+      print("Number of active jobs %d, Number of waiting jobs %d"%(len(actjobs),len(lefjobs)))
+      print("sep queue: %d R %d Q %d C"%(sepq['R'],sepq['Q'],sepq['C']))
+      print("2hr queue: %d R %d Q %d C\n"%(hr2q['R'],hr2q['Q'],hr2q['C']))
+
+    time.sleep(delay)
+
+def clean_clusterfiles(jobprefix,logpath,parpath,verb=True):
+  """
+  Cleans the files left behind from cluster submission
+
+  Parameters:
+    jobprefix - jobprefix for all files
+    logpath   - Path to the logfiles
+    parpath   - Path to the parfiles
+    verb      - verbosity flag [True]
+  """
+  # Remove scripts
+  rmscr = 'rm %s*.sh'%(jobprefix)
+  if(verb): print(rmscr)
+  sp = subprocess.check_call(rmscr,shell=True)
+  # Remove log files
+  rmlog = 'rm %s/%s*.log'%(logpath,jobprefix)
+  if(verb): print(rmlog)
+  sp = subprocess.check_call(rmlog,shell=True)
+  # Remove par files
+  rmpar = 'rm %s/%s*.par'%(parpath,jobprefix)
+  if(verb): print(rmpar)
+  sp = subprocess.check_call(rmpar,shell=True)
+  # Remove node files
+  rmnds = 'rm *-node.txt'
+  if(verb): print(rmnds)
+  sp = subprocess.check_call(rmnds,shell=True)
+  # Remove squeue.out
+  rmsq = 'rm squeue.out'
+  if(verb): print(rmsq)
+  sp = subprocess.check_call(rmsq,shell=True)
+  # Remove qstat.out
+  rmqs = 'rm qstat.out'
+  if(verb): print(rmqs)
+  sp = subprocess.check_call(rmqs,shell=True)
+
