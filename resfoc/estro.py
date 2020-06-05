ece68bac80e9071a6d272d3a534b4512667ff293
ke0m <joseph.s.jennings@gmail.com>
2020-06-05 08:02:52 -0700

    Merging angle mask and fault seg bug fix from bench-sep

--- resfoc/estro.py
+++ resfoc/estro.py
@@ -13,6 +13,7 @@ from resfoc.ssim import ssim
 from resfoc.rhoshifts import rhoshifts
 from scipy.ndimage import map_coordinates
 from scaas.trismooth import smooth
+from scaas.noise_generator import perlin
 
 def estro_angs(resang,oro,dro,agc=True,rect1semb=10,rect2semb=3,smooth=True,rect1pick=40,rect2pick=40,gate=3,an=1,niter=100):
   """
@@ -361,4 +362,67 @@ def ssim_ro(rimgs,fimg):
     ssims[iro] = ssim(rimgs[iro],fimg)
 
   return np.argmax(ssims)
+
+def anglemask(nz,na,zpos=None,apos=None,rectz=10,recta=10,mode='slant',rand=True):
+  """
+  Creates an angle mask that can be applied to an
+  angle gather to limit the number of angles (illumination)
+
+  Parameters:
+    nz    - number of depth samples
+    na    - number of angle samples
+    zpos  - percentage in z where to begin the mask [0.3]
+    apos  - percentage in a where to end the mask [0.6]
+    rectz - number of points to smooth the mask in z [10]
+    recta - number of points to smooth the mask in a [10]
+    mode  - mode of creating mask. Either a vertical mask ('vert') or slanted ['slant']
+    rand  - add smooth random variation to the mask [True]
+
+  Returns a single angle gather mask [nz,na]
+  """
+  # Create z(a) linear function
+  if(zpos is None):
+    z0 = int(0.3*nz)
+  else:
+    z0 = int(zpos*nz)
+  if(apos is None):
+    a0 = int(0.6*na)
+  else:
+    a0 = int(apos*na)
+
+  if(mode == 'vert'):
+    abeg = a0; aend = na-a0
+    mask = np.ones([nz,na])
+    mask[:,0:abeg] = 0.0
+    mask[:,aend:] = 0.0
+
+  elif(mode == 'slant'):
+    zf = nz-1; af = na-1
+
+    # Slope and intercept
+    m = (zf - z0)/(a0 - af)
+    b = (z0*a0 - zf*af)/(a0 - af)
+
+    a = np.array(range(a0,na))
+    zofa = (a*m + b).astype(int)
+
+    if(rand):
+      noise = 250*perlin(x=np.linspace(0,2,len(zofa)), octaves=2, persist=0.3, ncpu=1)
+      noise = noise - np.mean(noise)
+      zofa += noise.astype(int)
+
+    # Create mask
+    liner = np.ones([nz,na])
+    j = 0
+    for ia in a:
+      liner[zofa[j]:,ia] = 0
+      j += 1
+
+    # Flip for symmetry
+    linel = np.fliplr(liner)
+    mask = linel*liner
+
+  masksm = smooth(mask.astype('float32'),rect1=recta,rect2=rectz)
+
+  return masksm
 
