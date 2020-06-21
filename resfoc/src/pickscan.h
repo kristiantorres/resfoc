124142ca9c7fbcd5a59e1a62cf30ffba3fd7c2d9
ke0m <joseph.s.jennings@gmail.com>
2020-06-20 19:19:40 -0700

    New updates for semblance module

--- resfoc/src/pickscan.h
+++ resfoc/src/pickscan.h
@@ -3,7 +3,7 @@
  * A port of sfpick from Madagascar written by Sergey Fomel
  *
  * @author: Joseph Jennings
- * @version: 2020.06.19
+ * @version: 2020.06.20
  */
 
 #ifndef PICKSCAN_H_
@@ -11,8 +11,8 @@
 
 void normalize(int n1, int n2, float *scn);
 
-void pick(float an, int gate, bool norm, float vel0, float o2, float d2,
-          int n1, int n2, int n3, float *allscn, float *pck2, float *ampl, float *pcko);
+void pickscan(float an, int gate, bool norm, float vel0, float o2, float d2,
+              int n1, int n2, int n3, float *allscn, float *pck2, float *ampl, float *pcko);
 
 
 #endif /* PICKSCAN_H_ */
