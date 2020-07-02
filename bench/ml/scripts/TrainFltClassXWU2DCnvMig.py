33a6ac18682f012f79df77641a6d5e9da91b4239
ke0m <joseph.s.jennings@gmail.com>
2020-07-01 23:29:20 -0700

    CNN training notebook

---
+++ bench/ml/scripts/TrainFltClassXWU2DCnvMig.py
@@ -0,0 +1,143 @@
+"""
+Trains a CNN for segmenting faults on a seismic image
+using the architecture by Xinming Wu
+
+The neural network is of a U-net architecture (user can
+decide for skip connections or not). Network is implemented
+and trained with the Keras/Tensorflow deep learning framework.
+The training is done with the ADAM optimizer within Keras
+
+The input training data must be in H5 format. This program writes
+out the model architecture as well as the model weights
+and biases which can then be used subsequently for validation/testing.
+
+@author: Joseph Jennings
+@version: 2020.06.24
+"""
+import sys, os, argparse, configparser
+import inpout.seppy as seppy
+import numpy as np
+import h5py
+from deeplearn.dataloader import load_alldata
+from sklearn.utils import shuffle
+from deeplearn.kerasnets import unetxwu
+from deeplearn.kerascallbacks import F3Pred
+import tensorflow as tf
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+# Parse the config file
+conf_parser = argparse.ArgumentParser(add_help=False)
+conf_parser.add_argument("-c", "--conf_file",
+                         help="Specify config file", metavar="FILE")
+args, remaining_argv = conf_parser.parse_known_args()
+defaults = {
+    "verb": "y",
+    "nepochs": 10,
+    "nflts": 32,
+    "fltsize": 5,
+    "unet": "y",
+    "drpout": 0.0,
+    "gpus": []
+    }
+if args.conf_file:
+  config = configparser.ConfigParser()
+  config.read([args.conf_file])
+  defaults = dict(config.items("defaults"))
+
+# Parse the other arguments
+# Don't surpress add_help here so it will handle -h
+parser = argparse.ArgumentParser(parents=[conf_parser],description=__doc__,
+    formatter_class=argparse.RawDescriptionHelpFormatter)
+
+# Set defaults
+parser.set_defaults(**defaults)
+
+# Input files
+ioArgs = parser.add_argument_group('Inputs and outputs')
+ioArgs.add_argument("-cnvtrdat",help="Input convolution training dataset in H5 format",type=str,required=True)
+ioArgs.add_argument("-cnvvadat",help="Input convolution validation dataset in H5 format",type=str,required=True)
+ioArgs.add_argument("-migtrdat",help="Input migration training dataset in H5 format",type=str,required=True)
+ioArgs.add_argument("-migvadat",help="Input migration validation dataset in H5 format",type=str,required=True)
+ioArgs.add_argument("-wgtout",help="Output CNN filter coefficients",type=str)
+ioArgs.add_argument("-arcout",help="Output CNN architecture",type=str)
+ioArgs.add_argument("-chkpnt",help="Model checkpoint file",type=str)
+ioArgs.add_argument("-lssout",help="Output loss history",type=str)
+ioArgs.add_argument("-vlsout",help="Output validation loss history",type=str)
+ioArgs.add_argument("-accout",help="Output accuracy history",type=str)
+ioArgs.add_argument("-vacout",help="Output validation accuracy history",type=str)
+# Field data
+f3Args = parser.add_argument_group("Paths for analyzing field data predictions")
+f3Args.add_argument("-f3dat",help="Path to F3 data",type=str)
+f3Args.add_argument("-f3prd",help="Path for writing predictions on F3 data",type=str)
+f3Args.add_argument("-f3fig",help="Path for writing figures of predictions on F3 data",type=str)
+# Training
+trainArgs = parser.add_argument_group('Training parameters')
+trainArgs.add_argument('-bsize',help='Batch size [20]',type=int)
+trainArgs.add_argument('-nepochs',help='Number of passes over training data [10]',type=int)
+# Other arguments
+parser.add_argument("-verb",help="Verbosity flag ([y] or n)",type=str)
+parser.add_argument("-gpus",help="A comma delimited list of which GPUs to use [default all]",type=str)
+# Enables required arguments in config file
+for action in parser._actions:
+  if(action.dest in defaults):
+    action.required = False
+args = parser.parse_args(remaining_argv)
+
+# Set up SEP
+sep = seppy.sep(sys.argv)
+
+# Get command line arguments
+verb  = sep.yn2zoo(args.verb)
+gpus  = sep.read_list(args.gpus,[])
+if(len(gpus) != 0):
+  for igpu in gpus: os.environ['CUDA_VISIBLE_DEVICES'] = str(igpu)
+
+# Training arguments
+bsize   = args.bsize
+nepochs = args.nepochs
+
+# Load all data
+allcnvx,allcnvy = load_alldata(args.cnvtrdat,None,105,0,600)
+allmigx,allmigy = load_alldata(args.migtrdat,None,21,0,3000)
+
+# Combine the data
+allx = np.concatenate([allcnvx,allmigx],axis=0)
+ally = np.concatenate([allcnvy,allmigy],axis=0)
+xshape = allx.shape[1:]
+yshape = ally.shape[1:]
+
+allx,ally = shuffle(allx,ally,random_state=1992)
+
+model = unetxwu()
+
+# Set GPUs
+tf.compat.v1.GPUOptions(allow_growth=True)
+
+# Create callbacks
+checkpointer = ModelCheckpoint(filepath=args.chkpnt, verbose=1, save_best_only=True)
+f3predicter  = F3Pred(args.f3dat,args.f3prd,args.f3fig,105,(128,128),(64,64))
+
+# Train the model
+history = model.fit(allx,ally,epochs=nepochs,batch_size=bsize,verbose=1,shuffle=True,
+                   validation_split=0.2,callbacks=[checkpointer,f3predicter])
+
+# Write the model
+model.save_weights(args.wgtout)
+
+# Save the loss history
+lossvepch = np.asarray(history.history['loss'])
+sep.write_file(args.lssout,lossvepch)
+vlssvepch = np.asarray(history.history['val_loss'])
+sep.write_file(args.vlsout,vlssvepch)
+
+# Save the accuracy history
+accvepch = np.asarray(history.history['acc'])
+sep.write_file(args.accout,accvepch)
+vacvepch = np.asarray(history.history['val_acc'])
+sep.write_file(args.vacout,vacvepch)
+
+# Save the model architecture
+with open(args.arcout,'w') as f:
+  f.write(model.to_json())
+
---
+++ bench/ml/scripts/makedata_for_total.py
@@ -0,0 +1,75 @@
+import inpout.seppy as seppy
+import h5py
+import numpy as np
+from deeplearn.utils import resizepow2
+from deeplearn.dataloader import load_all_unlabeled_data, load_labeled_flat_data
+from sklearn.utils import shuffle
+import random
+from utils.ptyprint import create_inttag, progressbar
+
+sep = seppy.sep()
+
+# F3 dataset
+#iaxes,img = sep.read_file("./dat/f3cube.H")
+#img = img.reshape(iaxes.n,order='F')
+#img = np.ascontiguousarray(img.T).astype('float32')
+
+#iline = resizepow2(img[11]).T
+
+#np.save("f3iline.npy",iline)
+
+# Angle gather focus training data
+# Load all data
+nex = 5040
+nimgs = int(nex/105)
+focdat = load_all_unlabeled_data("/scr1/joseph29/angfaultfocptch.h5",0,nimgs)
+resdat = load_all_unlabeled_data("/scr1/joseph29/angfaultresptch.h5",0,nimgs)
+defdat,deflbl = load_labeled_flat_data("/scr1/joseph29/alldefocangs.h5",None,0,nex)
+
+## Size of each dataset
+nfoc = focdat.shape[0]
+nres = resdat.shape[0]
+ndef = defdat.shape[0]
+ndiff = nfoc - ndef
+
+# Make the three datasets the same size
+idxs1 = random.sample(range(nfoc), ndiff)
+idxs2 = random.sample(range(nres), ndiff)
+
+# Delete images randomly
+foctrm = np.delete(focdat,idxs1,axis=0)
+restrm = np.delete(resdat,idxs2,axis=0)
+
+## Remove half of each defocused and combine
+didxs1 = random.sample(range(ndef), int(ndef/2))
+didxs2 = random.sample(range(ndef), int(ndef/2))
+
+reshlf =  np.delete(restrm,didxs1,axis=0)
+defhlf =  np.delete(defdat,didxs2,axis=0)
+
+deftot = np.concatenate([reshlf,defhlf],axis=0)
+
+# Create labels for defocused and focused
+deflbls = np.zeros(ndef)
+foclbls = np.ones(ndef)
+
+# Concatenate focused and defocused images and labels
+allx = np.concatenate([deftot,foctrm],axis=0)
+ally = np.concatenate([deflbls,foclbls],axis=0)
+
+allx,ally = shuffle(allx,ally,random_state=1992)
+
+ntot = allx.shape[0]; na = allx.shape[1]; nz = allx.shape[2]; nx = allx.shape[2]
+
+# Write the labeled data to file
+hf = h5py.File('/scr1/joseph29/angfocus.h5','w')
+
+for iex in progressbar(range(ntot), "nex:"):
+  datatag = create_inttag(iex,ntot)
+  hf.create_dataset("x"+datatag, (na,nx,nz,1), data=allx[iex], dtype=np.float32)
+  hf.create_dataset("y"+datatag, (1,), data=ally[iex], dtype=np.float32)
+
+hf.close()
+
+# Angle mask
+
