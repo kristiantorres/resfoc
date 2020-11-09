0f99fb1b746e722fe6a9a04177120c6cdf075aa5
ke0m <joseph.s.jennings@gmail.com>
2020-11-09 00:08:25 -0800

    Merging fftw changes from master

--- resfoc/src/cosft.cpp
+++ resfoc/src/cosft.cpp
@@ -1,4 +1,6 @@
+#include <fftw3.h>
 #include "cosft.h"
+#include "kiss_fft.h"
 
 cosft::cosft(int n1_in) {
   /* Get sizes */
@@ -7,10 +9,14 @@ cosft::cosft(int n1_in) {
   _nw = _nt/2 + 1;
   /* Temporary arrays */
   _p = new float[_nt]();
-  _pp = new kiss_fft_cpx[_nw]();
+  _pp = new std::complex<float>[_nw]();
   /* Forward and inverse FFT objects */
-  _fwd = kiss_fftr_alloc(_nt,0,NULL,NULL);
-  _inv = kiss_fftr_alloc(_nt,1,NULL,NULL);
+  _fplan = fftwf_plan_dft_r2c_1d(_nt,
+                                 _p,reinterpret_cast<fftwf_complex*>(_pp),
+                                 FFTW_ESTIMATE);
+  _iplan = fftwf_plan_dft_c2r_1d(_nt,
+                                 reinterpret_cast<fftwf_complex*>(_pp),_p,
+                                 FFTW_ESTIMATE);
 }
 
 void cosft::fwd(float *q, int o1, int d1) {
@@ -26,26 +32,24 @@ void cosft::fwd(float *q, int o1, int d1) {
     _p[i] = _p[_nt-i];
   }
   /* Forward transform */
-  kiss_fftr(_fwd,_p,_pp);
+  fftwf_execute(_fplan);
   /* Copy real part */
   for(int i = 0; i < _n1; ++i) {
-    q[o1+i*d1] = _pp[i].r;
+    q[o1+i*d1] = std::real(_pp[i]);
   }
 }
 
 void cosft::inv(float *q, int o1, int d1) {
   /* Copy data */
   for(int i = 0; i < _n1; ++i) {
-    _pp[i].r = q[o1+i*d1];
-    _pp[i].i = 0.0;
+    _pp[i] = std::complex<float>(q[o1+i*d1],0.0);
   }
   /* Pad */
   for(int i = _n1; i < _nw; ++i) {
-    _pp[i].r = 0.0;
-    _pp[i].i = 0.0;
+    _pp[i] = std::complex<float>(0.0,0.0);
   }
   /* Inverse transform */
-  kiss_fftri(_inv,_pp,_p);
+  fftwf_execute(_iplan);
   /* Copy to output */
   for(int i = 0; i < _n1; ++i) {
     q[o1+i*d1] = _p[i]/_nt;
