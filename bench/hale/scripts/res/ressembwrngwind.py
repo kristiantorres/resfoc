5d7ca720be17cb2b107f8a6456c8c31325e4c1a0
ke0m <joseph.s.jennings@gmail.com>
2020-11-08 23:36:29 -0800

    Tried to mute the gathers. Did not change the semblance result

--- bench/hale/scripts/res/ressembwrngwind.py
+++ bench/hale/scripts/res/ressembwrngwind.py
@@ -7,7 +7,8 @@ from genutils.plot import plot_rhopicks, plot_anggatrhos, plot_rhoimg2d
 sep = seppy.sep()
 
 # Read in residually migrated gathers
-saxes,storm = sep.read_file("resfaultfocuswindt.H")
+#saxes,storm = sep.read_file("resfaultfocuswindt.H")
+saxes,storm = sep.read_file("resfaultfocuswindtmute.H")
 nz,na,nx,nro = saxes.n; oz,oa,ox,oro = saxes.o; dz,da,dx,dro = saxes.d
 storm = storm.reshape(saxes.n,order='F').T
 
@@ -25,13 +26,13 @@ stkw = np.sum(stormw,axis=2)
 sc2 = 0.2
 kmin = sc2*np.min(stkw); kmax= sc2*np.max(stkw)
 
-fsize = 16
-for ix in range(0,nx,50):
-  plot_anggatrhos(stormw,ix,dz,dx,oro,dro,ox=ox,show=False,pclip=0.6,fontsize=fsize,ticksize=fsize,
-                  imgaspect=2.0,roaspect=0.02)
-  # Plot the picked
-  plot_rhopicks(stormw[:,ix,:,:],semb[ix,:,:],rho[ix,:],dro,dz,oro,show=True,angaspect=0.02,
-                vmin=smin,vmax=smax,wspace=0.1,rhoaspect=0.08,pclip=1.1)
+#fsize = 16
+#for ix in range(0,nx,50):
+#  plot_anggatrhos(stormw,ix,dz,dx,oro,dro,ox=ox,show=False,pclip=0.6,fontsize=fsize,ticksize=fsize,
+#                  imgaspect=2.0,roaspect=0.02)
+#  # Plot the picked
+#  plot_rhopicks(stormw[:,ix,:,:],semb[ix,:,:],rho[ix,:],dro,dz,oro,show=True,angaspect=0.02,
+#                vmin=smin,vmax=smax,wspace=0.1,rhoaspect=0.08,pclip=1.1)
 
 # Refocus the stack
 rfi = refocusimg(stkw,rho,dro)
@@ -46,9 +47,9 @@ rfaw  = rfa[:,:,100:356]
 # Window to the target region
 plot_rhoimg2d(stkww.T,rhow.T,dx=dx,dz=dz,ox=ox,oz=100*dz,aspect=2.0)
 
-sep.write_file("faultfocussembwind.H",sembw.T,os=[oz,oro],ds=[dz,dro])
-sep.write_file("faultfocusrhowind.H",rhow.T,os=[oz,ox],ds=[dz,dx])
-sep.write_file("faultfocusrfiwind.H",rfiw.T,os=[oz,ox],ds=[dz,dx])
-sep.write_file("faultfocusstkwind.H",stkww.T,os=[oz,ox],ds=[dz,dx])
-sep.write_file("faultfocusrfawind.H",rfaw.T,os=[oz,0,ox],ds=[dz,da,dx])
+sep.write_file("faultfocussembmutwind.H",sembw.T,os=[oz,oro],ds=[dz,dro])
+sep.write_file("faultfocusrhomutwind.H",rhow.T,os=[oz,ox],ds=[dz,dx])
+sep.write_file("faultfocusrfimutwind.H",rfiw.T,os=[oz,ox],ds=[dz,dx])
+sep.write_file("faultfocusstkmutwind.H",stkww.T,os=[oz,ox],ds=[dz,dx])
+sep.write_file("faultfocusrfamutwind.H",rfaw.T,os=[oz,0,ox],ds=[dz,da,dx])
 
