ffa41d22f349a53f122f53f5ad00e3bd057b4059
ke0m <joseph.s.jennings@gmail.com>
2020-12-05 23:58:26 -0800

    Muting the full aperture dataset

--- bench/f3/scripts/proc/f3_qc.py
+++ bench/f3/scripts/proc/f3_qc.py
@@ -7,7 +7,8 @@ import matplotlib.pyplot as plt
 
 # Data
 sep = seppy.sep()
-saxes,sht = sep.read_file("/data3/northsea_dutch_f3/f3_shots2.H")
+saxes,sht = sep.read_file("/data3/northsea_dutch_f3/f3_shots2_full.H")
+#saxes,sht = sep.read_file("f3_shots2_full_muted.H")
 #saxes,sht = sep.read_file("f3_shots2_muted.H")
 #saxes,sht = sep.read_file("f3_shots2_muted_debub_onetr.H")
 #saxes,sht = sep.read_file("f3_shots2_muted_debub_shot.H")
@@ -16,21 +17,20 @@ ntr,nt = sht.shape
 dt = 0.002
 
 # Geometry
-sxaxes,srcx = sep.read_file("/data3/northsea_dutch_f3/f3_srcx2.H")
-syaxes,srcy = sep.read_file("/data3/northsea_dutch_f3/f3_srcy2.H")
-rxaxes,recx = sep.read_file("/data3/northsea_dutch_f3/f3_recx2.H")
-ryaxes,recy = sep.read_file("/data3/northsea_dutch_f3/f3_recy2.H")
+sxaxes,srcx = sep.read_file("/data3/northsea_dutch_f3/f3_srcx2_full.H")
+syaxes,srcy = sep.read_file("/data3/northsea_dutch_f3/f3_srcy2_full.H")
+rxaxes,recx = sep.read_file("/data3/northsea_dutch_f3/f3_recx2_full.H")
+ryaxes,recy = sep.read_file("/data3/northsea_dutch_f3/f3_recy2_full.H")
 
-naxes,nrec = sep.read_file("/data3/northsea_dutch_f3/f3_nrec2.H")
+naxes,nrec = sep.read_file("/data3/northsea_dutch_f3/f3_nrec2_full.H")
 nrec = nrec.astype('int32')
-nsht = 1625
+nsht = 3250
 nd = np.sum(nrec[:nsht])
 
-# Migration
-maxes,mig = sep.read_file("/data3/northsea_dutch_f3/mig/mig.T")
-mig = mig.reshape(maxes.n,order='F')
-migw  = mig[:,200:1200,5:505]
-migslc = migw[400]
+# Read in time slice for QC
+saxes,slc = sep.read_wind("migwt.T",fw=400,nw=1)
+dy,dx,dt = saxes.d; oy,ox,ot = saxes.o
+slc = slc.reshape(saxes.n,order='F').T
 
-qc_f3data(sht[:nd],srcx[:nsht],recx[:nd],srcy[:nsht],recy[:nd],nrec[:nsht],migslc,pclip=0.02)
+qc_f3data(sht[:nd],srcx[:nsht],recx[:nd],srcy[:nsht],recy[:nd],nrec[:nsht],slc,pclip=0.02)
 
