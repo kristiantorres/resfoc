84e75b5acb328d16729224c72ca3c66b8a3d518f
ke0m <joseph.s.jennings@gmail.com>
2020-12-13 00:03:49 -0800

    gun and cable correction for four ms data

---
+++ bench/f3/scripts/proc/four/gnc_correction.py
@@ -0,0 +1,21 @@
+import inpout.seppy as seppy
+import numpy as np
+from genutils.plot import plot_dat2d
+
+sep = seppy.sep()
+
+daxes,dat = sep.read_file("f3_shots2interp_muted_debub_onetr.H")
+dat = np.ascontiguousarray(dat.reshape(daxes.n,order='F').T).astype('float32')
+
+datp = np.pad(dat,((0,0),(0,2)))
+
+# Shift by 8 milliseconds
+datpm = np.roll(datp,2,axis=1)
+
+# Window to 6 s
+datpmw = datpm[:,:1500]
+
+plot_dat2d(dat[:1000],dt=0.004,pclip=0.1,show=False,aspect=50.0)
+plot_dat2d(datpm[:1000],dt=0.004,pclip=0.1,show=True,aspect=50.0)
+
+sep.write_file("f3_shots2interp_muted_debub_onetr_gncw.H",datpmw.T,ds=daxes.d,os=daxes.o)
