2b789ffa0d5f224f51963b3bbac1ad8aaf848cfc
ke0m <joseph.s.jennings@gmail.com>
2020-12-29 23:53:19 -0800

    QCing the new cleaned up dataset

--- bench/f3/scripts/plot/plot_acqfull.py
+++ bench/f3/scripts/plot/plot_acqfull.py
@@ -5,12 +5,12 @@ from seis.f3utils import plot_acq
 sep = seppy.sep()
 
 # Read in geometry
-sxaxes,srcx = sep.read_file("/data3/northsea_dutch_f3/windowed_data/f3_srcx3_full.H")
-syaxes,srcy = sep.read_file("/data3/northsea_dutch_f3/windowed_data/f3_srcy3_full.H")
-rxaxes,recx = sep.read_file("/data3/northsea_dutch_f3/windowed_data/f3_recx3_full.H")
-ryaxes,recy = sep.read_file("/data3/northsea_dutch_f3/windowed_data/f3_recy3_full.H")
+sxaxes,srcx = sep.read_file("f3_srcx3_full_clean.H")
+syaxes,srcy = sep.read_file("f3_srcy3_full_clean.H")
+rxaxes,recx = sep.read_file("f3_recx3_full_clean.H")
+ryaxes,recy = sep.read_file("f3_recy3_full_clean.H")
 
-naxes,nrec = sep.read_file("/data3/northsea_dutch_f3/windowed_data/f3_nrec3_full.H")
+naxes,nrec = sep.read_file("f3_nrec3_full_clean.H")
 nrec = nrec.astype('int32')
 
 srcx *= 0.001
--- bench/f3/scripts/proc/four/f3_qc.py
+++ bench/f3/scripts/proc/four/f3_qc.py
@@ -11,20 +11,20 @@ sep = seppy.sep()
 #saxes,sht = sep.read_file("f3_shots2interp_muted_debub_onetr.H")
 #saxes,sht = sep.read_file("f3_shots2interp_full_muted.H")
 #saxes,sht = sep.read_file("f3_shots2interp_full_muted_debub_onetr.H")
-saxes,sht = sep.read_file("f3_shots3interp_full.H")
+saxes,sht = sep.read_file("f3_shots3interp_full_clean.H")
 sht = np.ascontiguousarray(sht.reshape(saxes.n,order='F').T).astype('float32')
 ntr,nt = sht.shape
 dt = 0.004
 
 # Geometry
-sxaxes,srcx = sep.read_file("/data3/northsea_dutch_f3/windowed_data/f3_srcx3_full.H")
-syaxes,srcy = sep.read_file("/data3/northsea_dutch_f3/windowed_data/f3_srcy3_full.H")
-rxaxes,recx = sep.read_file("/data3/northsea_dutch_f3/windowed_data/f3_recx3_full.H")
-ryaxes,recy = sep.read_file("/data3/northsea_dutch_f3/windowed_data/f3_recy3_full.H")
+sxaxes,srcx = sep.read_file("f3_srcx3_full_clean.H")
+syaxes,srcy = sep.read_file("f3_srcy3_full_clean.H")
+rxaxes,recx = sep.read_file("f3_recx3_full_clean.H")
+ryaxes,recy = sep.read_file("f3_recy3_full_clean.H")
 
-naxes,nrec = sep.read_file("/data3/northsea_dutch_f3/windowed_data/f3_nrec3_full.H")
+naxes,nrec = sep.read_file("f3_nrec3_full_clean.H")
 nrec = nrec.astype('int32')
-nsht = 15625
+nsht = 15461
 nd = np.sum(nrec[:nsht])
 
 # Read in time slice for QC
